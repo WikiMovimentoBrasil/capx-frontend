7d8a00c7e2646c4db764b0606cd4a227b624b0de
yuritavares <yuriatp@gmail.com>
2024-03-25 15:24:24 -0300

    Configuring main tag from home.

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
 export default function Home() {
   return (
-    <main></main>
+    <main className="flex flex-wrap flex-col w-full bg-zinc-50 font-montserrat text-capx-secondary-gray"></main>
   );
 }
