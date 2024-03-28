2c7102dbb2b907e9de5966761de8c67f3af70690
yuritavares <yuriatp@gmail.com>
2024-03-28 12:30:01 -0300

    Profile: Adding TextArea to ProfileForm.

--- src/app/profile/components/ProfileForm.js
+++ src/app/profile/components/ProfileForm.js
@@ -1,3 +1,4 @@
+import TextArea from "./TextArea";
 import TextInput from "./TextInput";
 
 export default function ProfileForm() {
@@ -5,6 +6,7 @@ export default function ProfileForm() {
     <section className="flex flex-wrap flex-col w-10/12 mx-auto sm:mt-52 sm:pb-32 font-montserrat">
       <TextInput>Display name</TextInput>
       <TextInput>Profile image</TextInput>
+      <TextArea>About me</TextArea>
       <TextInput>X</TextInput>
       <TextInput>Facebook</TextInput>
       <TextInput>Instagram</TextInput>
