cc06ebd95a84c6b95725be20fd518c34c6475b59
yuritavares <yuriatp@gmail.com>
2024-03-28 12:29:36 -0300

    Profile: Adding TextInput to ProfileForm.

--- src/app/profile/components/ProfileForm.js
+++ src/app/profile/components/ProfileForm.js
@@ -1,5 +1,18 @@
+import TextInput from "./TextInput";
+
 export default function ProfileForm() {
   return (
-    <section className="flex flex-wrap flex-col w-10/12 mx-auto sm:mt-52 sm:pb-32 font-montserrat"></section>
+    <section className="flex flex-wrap flex-col w-10/12 mx-auto sm:mt-52 sm:pb-32 font-montserrat">
+      <TextInput>Display name</TextInput>
+      <TextInput>Profile image</TextInput>
+      <TextInput>X</TextInput>
+      <TextInput>Facebook</TextInput>
+      <TextInput>Instagram</TextInput>
+      <TextInput>Telegram</TextInput>
+      <TextInput>Github</TextInput>
+      <TextInput>IRC</TextInput>
+      <TextInput>Wikimedia alternative account</TextInput>
+      <TextInput>Wikimedia developer account</TextInput>
+    </section>
   )
 }
