5012b949c5e3ce045832b42998dcd5f6ab8369e4
yuritavares <yuriatp@gmail.com>
2024-03-28 12:18:48 -0300

    Profile: Adding style and animation to MultiSelectInput component.

--- src/app/profile/components/MultiSelectInput.js
+++ src/app/profile/components/MultiSelectInput.js
@@ -1,5 +1,25 @@
 "use client";
 import Select from "react-select";
+import makeAnimated from 'react-select/animated';
+
+const animatedComponents = makeAnimated();
+const multiSelectInput = {
+  control: (base, state) => ({
+    ...base,
+    height: '48px',
+    paddingLeft: '4px',
+    color: 'rgb(30 41 59)',
+    border: state.isFocused ? 0 : 0,
+    // This line disable the blue border
+    boxShadow: state.isFocused ? 0 : 0,
+    "&:hover": {
+      border: state.isFocused ? 0 : 0
+    },
+  }),
+  placeholder: (styles) => ({
+    ...styles,
+  }),
+};
 
 export default function MultiSelectInput({ children }) {
   return (
@@ -8,8 +28,10 @@ export default function MultiSelectInput({ children }) {
       <Select
         isMulti
         isSearchable={true}
+        styles={multiSelectInput}
         placeholder={""}
         noOptionsMessage={() => "No more options to select."}
+        components={animatedComponents}
         className="border rounded-md"
       />
     </div>
