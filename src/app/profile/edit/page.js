86bf1727605f7b2374bcfd55df397357f31fb47b
yuritavares <yuriatp@gmail.com>
2024-03-28 12:31:37 -0300

    Profile: Adding ProfileForm component to page.

--- src/app/profile/edit/page.js
+++ src/app/profile/edit/page.js
@@ -1,9 +1,11 @@
 import Navbar from "@/components/Navbar";
+import ProfileForm from "../components/ProfileForm";
 
 export default function ProfileEdit() {
   return (
     <main className="flex flex-wrap flex-col w-full h-screen bg-zinc-50 font-montserrat text-capx-secondary-gray">
       <Navbar></Navbar>
+      <ProfileForm></ProfileForm>
     </main>
   )
 }
