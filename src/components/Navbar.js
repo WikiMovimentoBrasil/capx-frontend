d0543d3ded578996797c48942b1eaf813de52433
yuritavares <yuriatp@gmail.com>
2024-03-28 18:24:07 -0300

    Navbar: Starting Navbar variation based on user login.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,7 @@ import capXLogo from "../../public/static/images/capx_logo.svg";
 // Navbar component has two parts:
 // 1. Capacity Exchange Logo on the left.
 // 2. Menu on the right.
-export default function Navbar() {
+export default function Navbar({ loggedInUser }) {
   return (
     <nav className="absolute w-10/12 h-fit m-auto left-0 right-0 mt-10">
       <div className="flex w-full place-content-between place-items-end sm:items-center">
@@ -20,36 +20,40 @@ export default function Navbar() {
             className="w-full sm:w-16"
           />
         </NextLink>
-        <div className="flex sm:items-center space-x-12">
-          {/* About button */}
-          <Link
-            activeClass="active"
-            to="section02"
-            spy={true}
-            smooth={true}
-            duration={500}
-            delay={150}
-            className="cursor-pointer hover:underline"
-          >
-            About
-          </Link>
-          {/* How it works button */}
-          <Link
-            activeClass="active"
-            to="section03"
-            spy={true}
-            smooth={true}
-            duration={500}
-            delay={150}
-            className="cursor-pointer hover:underline"
-          >
-            How it works
-          </Link>
-          {/* Login button */}
-          <NextLink href="/terms" className="flex w-fit h-fit bg-capx-secondary-green hover:bg-capx-primary-green text-white px-2 sm:px-8 py-2 rounded-full select-none items-center">
-            Login
-          </NextLink>
-        </div>
+        {loggedInUser ? (
+          null
+        ) : (
+          <div className="flex sm:items-center space-x-12">
+            {/* About button */}
+            <Link
+              activeClass="active"
+              to="section02"
+              spy={true}
+              smooth={true}
+              duration={500}
+              delay={150}
+              className="cursor-pointer hover:underline"
+            >
+              About
+            </Link>
+            {/* How it works button */}
+            <Link
+              activeClass="active"
+              to="section03"
+              spy={true}
+              smooth={true}
+              duration={500}
+              delay={150}
+              className="cursor-pointer hover:underline"
+            >
+              How it works
+            </Link>
+            {/* Login button */}
+            <NextLink href="/terms" className="flex w-fit h-fit bg-capx-secondary-green hover:bg-capx-primary-green text-white px-2 sm:px-8 py-2 rounded-full select-none items-center">
+              Login
+            </NextLink>
+          </div>
+        )}
       </div>
     </nav>
   );
