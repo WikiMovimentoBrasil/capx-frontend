f99bcf794da6cec4eaa9d73a568c47dc2991d799
yuritavares <yuriatp@gmail.com>
2024-03-27 23:22:05 -0300

    Section01: Adding wikimedians image.

--- src/components/Section01.js
+++ src/components/Section01.js
@@ -1,3 +1,6 @@
+import Image from "next/image";
+import WikimediansImage from "../../public/static/images/s01wikimedians.png";
+
 export default function Section01() {
   return (
     <section className="flex w-10/12 h-screen mx-auto my-auto">
@@ -8,6 +11,13 @@ export default function Section01() {
         <p className="sm:text-lg">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec commodo sollicitudin lectus non rhoncus. In hac habitasse platea dictumst. Fusce porttitor facilisis scelerisque.
         </p>
+      </div>
+      <div className="flex flex-wrap sm:w-1/2 sm:h-full">
+        <Image
+          src={WikimediansImage}
+          alt="Three wikimedians using Capacity Exchange."
+          className="flex mx-auto my-auto sm:w-full sm:pl-24 sm:max-w-[650px] drop-shadow-md"
+        />
       </div>
     </section>
   )
