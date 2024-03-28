b242c0892c9ae9bb6f7c45c6d984e9415b352047
yuritavares <yuriatp@gmail.com>
2024-03-27 23:29:15 -0300

    Adding Section01.

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import Navbar from "@/components/Navbar";
+import Section01 from "@/components/Section01";
 
 export default function Home() {
   return (
     <main className="flex flex-wrap flex-col w-full bg-zinc-50 font-montserrat text-capx-secondary-gray">
       <Navbar></Navbar>
+      <Section01></Section01>
     </main>
   );
 }
