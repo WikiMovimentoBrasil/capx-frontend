37c5621c76e0e51c25960d8d0fcb78aea96b0bbb
yuritavares <yuriatp@gmail.com>
2024-03-28 12:30:35 -0300

    Profile: Adding SelectInput to ProfileForm.

--- src/app/profile/components/ProfileForm.js
+++ src/app/profile/components/ProfileForm.js
@@ -1,9 +1,11 @@
 import TextArea from "./TextArea";
 import TextInput from "./TextInput";
+import SelectInput from "./SelectInput";
 
 export default function ProfileForm() {
   return (
     <section className="flex flex-wrap flex-col w-10/12 mx-auto sm:mt-52 sm:pb-32 font-montserrat">
+      <SelectInput>Pronoun</SelectInput>
       <TextInput>Display name</TextInput>
       <TextInput>Profile image</TextInput>
       <TextArea>About me</TextArea>
@@ -15,6 +17,8 @@ export default function ProfileForm() {
       <TextInput>IRC</TextInput>
       <TextInput>Wikimedia alternative account</TextInput>
       <TextInput>Wikimedia developer account</TextInput>
+      <SelectInput>Preferred contact method</SelectInput>
+      <SelectInput>Region</SelectInput>
     </section>
   )
 }
