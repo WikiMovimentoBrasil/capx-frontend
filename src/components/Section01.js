bfba1915b833e8a1b1222d901f54c3dbd6fa1496
yuritavares <yuriatp@gmail.com>
2024-03-27 23:27:38 -0300

    Section01: Dividing title into two tags.

--- src/components/Section01.js
+++ src/components/Section01.js
@@ -4,9 +4,12 @@ import WikimediansImage from "../../public/static/images/s01wikimedians.png";
 export default function Section01() {
   return (
     <section className="flex w-10/12 h-screen mx-auto my-auto">
-      <div className="flex flex-wrap place-content-center sm:w-1/2 sm:h-5/6">
-        <h1 className="font-chunkfive mb-2 sm:text-5xl">
-          Exchanging capacities, from wikimedians to wikimedians
+      <div className="flex flex-wrap place-content-center text-left sm:w-1/2 sm:h-5/6">
+        <h1 className="sm:w-full font-chunkfive sm:text-5xl mb-2">
+          Exchanging capacities,
+        </h1>
+        <h1 className="sm:w-full font-chunkfive sm:text-5xl mb-2">
+          from wikimedians to wikimedians
         </h1>
         <p className="sm:text-lg">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec commodo sollicitudin lectus non rhoncus. In hac habitasse platea dictumst. Fusce porttitor facilisis scelerisque.
