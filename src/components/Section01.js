f6b497a1698c0922549942997d65f2a1e9472f8b
yuritavares <yuriatp@gmail.com>
2024-03-27 23:20:53 -0300

    Section01: Creating title and description.

--- src/components/Section01.js
+++ src/components/Section01.js
@@ -1,5 +1,14 @@
 export default function Section01() {
   return (
-    <section className="flex w-10/12 h-screen mx-auto my-auto"></section>
+    <section className="flex w-10/12 h-screen mx-auto my-auto">
+      <div className="flex flex-wrap place-content-center sm:w-1/2 sm:h-5/6">
+        <h1 className="font-chunkfive mb-2 sm:text-5xl">
+          Exchanging capacities, from wikimedians to wikimedians
+        </h1>
+        <p className="sm:text-lg">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec commodo sollicitudin lectus non rhoncus. In hac habitasse platea dictumst. Fusce porttitor facilisis scelerisque.
+        </p>
+      </div>
+    </section>
   )
 }
