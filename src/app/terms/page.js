0dda646ddcf5acd447b2e3676b5abd6f179eb92d
yuritavares <yuriatp@gmail.com>
2024-03-28 00:40:42 -0300

    Terms: Adding title, terms description and login button.

--- src/app/terms/page.js
+++ src/app/terms/page.js
@@ -14,6 +14,15 @@ export default function Terms() {
             className="w-full sm:w-32"
           />
         </Link>
+        <h2 className="w-full mb-2 sm:text-xl">
+          Before you log in
+        </h2>
+        <p className="w-full mb-8">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla id aliquam sem, non congue sapien. Donec vel cursus erat. Sed sagittis felis nec dignissim pulvinar. Nunc blandit convallis porta. Proin vel ligula non tellus interdum gravida a vitae arcu. Nulla non molestie sapien. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed ultricies ipsum ut fermentum pretium. Sed ultrices varius auctor. Ut interdum et arcu id convallis. Aliquam erat volutpat.
+        </p>
+        <Link href="/" className="w-full bg-capx-secondary-green hover:bg-capx-primary-green text-white px-2 sm:px-8 py-2 rounded-full text-center">
+          Login
+        </Link>
       </div>
     </main>
   )
