f3cc497e01688053285fc96a2636a2cb572d5c85
yuritavares <yuriatp@gmail.com>
2024-04-08 20:40:47 -0300

    Section01: Starting desktop version.

--- src/components/Section01.js
+++ src/components/Section01.js
@@ -3,13 +3,14 @@ import WikimedianImage from "../../public/static/images/s01wikimedian.png";
 
 export default function Section01() {
   return (
-    <section className={"flex flex-wrap w-10/12 h-fit mx-auto my-auto place-content-start py-32 font-montserrat"}>
-      <h1 className="w-full h-fit font-extrabold text-3xl leading-10 mb-8">A space for<br></br>knowledge exchange</h1>
-      <div className="grid w-full justify-items-end">
+    <section className={"flex flex-wrap sm:flex-nowrap w-10/12 h-fit sm:h-screen mx-auto place-content-start py-32 font-montserrat"}>
+      {/* Mobile Title */}
+      <h1 className="block sm:hidden w-full h-fit font-extrabold text-3xl leading-10 mb-8">A space to<br></br>exchange knowledge</h1>
+      <div className="grid w-full justify-items-end sm:w-1/3 sm:place-content-center">
         <Image
           src={WikimedianImage}
           alt="Wikimedians using Capacity Exchange."
-          className="w-10/12 h-fit"
+          className="w-10/12 sm:w-full h-fit"
         />
       </div>
       <h3 className="w-full h-fit text-2xl font-regular leading-8 mb-8">
