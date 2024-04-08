6a7b25b2dba67a06d093820013d044463feaf4ce
yuritavares <yuriatp@gmail.com>
2024-04-08 18:56:41 -0300

    Section01: Adding main image.

--- src/components/Section01.js
+++ src/components/Section01.js
@@ -1,7 +1,17 @@
+import Image from "next/image";
+import WikimedianImage from "../../public/static/images/s01wikimedian.png";
+
 export default function Section01() {
   return (
     <section className={"flex flex-wrap w-10/12 h-fit mx-auto my-auto place-content-start py-32 font-montserrat"}>
       <h1 className="w-full h-fit font-extrabold text-3xl leading-10 mb-8">A space for<br></br>knowledge exchange</h1>
+      <div className="grid w-full justify-items-end">
+        <Image
+          src={WikimedianImage}
+          alt="Wikimedians using Capacity Exchange."
+          className="w-10/12 h-fit"
+        />
+      </div>
     </section>
   )
 }
