621546dceb3a1864383611e6f4bb0b2d46062b2a
yuritavares <yuriatp@gmail.com>
2024-04-08 18:56:21 -0300

    Section01: Adding main title.

--- src/components/Section01.js
+++ src/components/Section01.js
@@ -1,5 +1,7 @@
 export default function Section01() {
   return (
-    <section className="flex flex-wrap w-10/12 h-fit mx-auto my-auto place-content-start py-32 font-montserrat"></section>
+    <section className={"flex flex-wrap w-10/12 h-fit mx-auto my-auto place-content-start py-32 font-montserrat"}>
+      <h1 className="w-full h-fit font-extrabold text-3xl leading-10 mb-8">A space for<br></br>knowledge exchange</h1>
+    </section>
   )
 }
