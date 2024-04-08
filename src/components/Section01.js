a03163611c75125cf289587149a4c696b9d7aada
yuritavares <yuriatp@gmail.com>
2024-04-08 18:55:34 -0300

    Section01: Restarting component with light/dark mode.

--- src/components/Section01.js
+++ src/components/Section01.js
@@ -1,27 +1,5 @@
-import Image from "next/image";
-import WikimediansImage from "../../public/static/images/s01wikimedians.png";
-
 export default function Section01() {
   return (
-    <section className="flex w-10/12 h-screen mx-auto my-auto">
-      <div className="flex flex-wrap place-content-center text-left sm:w-1/2 sm:h-5/6">
-        <h1 className="sm:w-full font-chunkfive sm:text-5xl mb-2">
-          Exchanging capacities,
-        </h1>
-        <h1 className="sm:w-full font-chunkfive sm:text-5xl mb-2">
-          from wikimedians to wikimedians
-        </h1>
-        <p className="sm:text-lg">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec commodo sollicitudin lectus non rhoncus. In hac habitasse platea dictumst. Fusce porttitor facilisis scelerisque.
-        </p>
-      </div>
-      <div className="flex flex-wrap sm:w-1/2 sm:h-full">
-        <Image
-          src={WikimediansImage}
-          alt="Three wikimedians using Capacity Exchange."
-          className="flex mx-auto my-auto sm:w-full sm:pl-24 sm:max-w-[650px] drop-shadow-md"
-        />
-      </div>
-    </section>
+    <section className="flex flex-wrap w-10/12 h-fit mx-auto my-auto place-content-start py-32 font-montserrat"></section>
   )
 }
