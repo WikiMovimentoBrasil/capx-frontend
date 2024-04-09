923ba969e133e7b120fb5f5a5e4b78e674b65cf2
yuritavares <yuriatp@gmail.com>
2024-04-09 11:06:30 -0300

    Creating state to use light/dark mode.

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,14 @@
+"use client";
 import Navbar from "@/components/Navbar";
 import Section01 from "@/components/Section01";
+import { useState } from "react";
 
 export default function Home() {
+  const [darkMode, setDarkMode] = useState(false);
+
   return (
-    <main className="flex flex-wrap flex-col w-full bg-zinc-50 font-montserrat text-capx-secondary-gray">
-      <Navbar></Navbar>
+    <main className={(darkMode ? "bg-capx-dark-bg text-capx-light-bg " : "bg-capx-light-bg text-capx-dark-bg ") + " flex flex-wrap flex-col w-full font-montserrat"}>
+      <Navbar darkMode={darkMode} setDarkMode={setDarkMode}></Navbar>
       <Section01></Section01>
     </main>
   );
