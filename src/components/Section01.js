0ec12dfb078d4623cb5185c8ed19275eb2593701
yuritavares <yuriatp@gmail.com>
2024-04-09 10:43:26 -0300

    Section01: Removing mobile description to use just one.

--- src/components/Section01.js
+++ src/components/Section01.js
@@ -8,10 +8,6 @@ export default function Section01() {
         <h3 className="hidden sm:block w-full h-fit text-3xl font-regular mb-14">
           Connect with peers, learn and share capacity in a platform made for and by the Wikimedia Movement.
         </h3>
-        {/* Mobile Description */}
-        <h3 className="block sm:hidden w-full h-fit text-2xl font-regular leading-8 mb-8">
-          Connect<br></br>with peers,<br></br>learn and share<br></br>capacity in a platform<br></br>made for and by the<br></br>Wikimedia Movement.
-        </h3>
         <div className="w-full">
           <h2 className="w-fit bg-capx-secondary-purple font-extrabold text-xl sm:text-2xl text-[#F6F6F6] mx-auto px-10 py-4 rounded-full">Coming Soon</h2>
         </div>
