00a721c1c3605bd2c235ec9445d30549d820628a
yuritavares <yuriatp@gmail.com>
2024-04-09 11:07:35 -0300

    Adding Section02 and Section03 components to project.

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,8 @@
 "use client";
 import Navbar from "@/components/Navbar";
 import Section01 from "@/components/Section01";
+import Section02 from "@/components/Section02";
+import Section03 from "@/components/Section03";
 import { useState } from "react";
 
 export default function Home() {
@@ -10,6 +12,8 @@ export default function Home() {
     <main className={(darkMode ? "bg-capx-dark-bg text-capx-light-bg " : "bg-capx-light-bg text-capx-dark-bg ") + " flex flex-wrap flex-col w-full font-montserrat"}>
       <Navbar darkMode={darkMode} setDarkMode={setDarkMode}></Navbar>
       <Section01></Section01>
+      <Section02 darkMode={darkMode}></Section02>
+      <Section03></Section03>
     </main>
   );
 }
