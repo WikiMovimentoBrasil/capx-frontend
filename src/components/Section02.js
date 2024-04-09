2c332124c7ed18707815d1bd654f98821d8e5392
yuritavares <yuriatp@gmail.com>
2024-04-09 10:50:50 -0300

    Section02: Checking dark mode to apply custom bg.

--- src/components/Section02.js
+++ src/components/Section02.js
@@ -1,5 +1,5 @@
-export default function Section02() {
+export default function Section02({ darkMode }) {
   return (
-    <section id="section02" className="flex w-full h-screen"></section>
+    <section id="section02" className={(darkMode ? "bg-capx-dark-box-bg " : "bg-capx-light-box-bg ") + "flex w-full h-screen"}></section>
   )
 }
