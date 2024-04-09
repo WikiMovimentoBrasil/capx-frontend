02d7e2aedb3da5f208e8d9adae4b9b95e87af87d
yuritavares <yuriatp@gmail.com>
2024-04-09 10:57:41 -0300

    Section02: Adding mobile text.

--- src/components/Section02.js
+++ src/components/Section02.js
@@ -1,5 +1,10 @@
 export default function Section02({ darkMode }) {
   return (
-    <section id="section02" className={(darkMode ? "bg-capx-dark-box-bg " : "bg-capx-light-box-bg ") + "flex w-full h-screen"}></section>
+    <section id="section02" className={(darkMode ? "bg-capx-dark-box-bg " : "bg-capx-light-box-bg ") + "flex w-full h-screen"}>
+      <div className="flex flex-wrap place-content-center w-10/12 h-screen mx-auto my-auto">
+        {/* Mobile Text */}
+        <h2 className="block sm:hidden font-extrabold text-2xl text-center mb-10">Follow up on<br></br>meta-wikimedia:</h2>
+      </div>
+    </section>
   )
 }
