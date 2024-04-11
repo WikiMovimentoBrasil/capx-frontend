ef44aab6b2d9668fe0f7e3ed71c3552bd144ab56
yuritavares <yuriatp@gmail.com>
2024-04-11 19:50:55 -0300

    BackToTopButton: Necessary imports.

--- src/components/BackToTopButton.js
+++ src/components/BackToTopButton.js
@@ -1,3 +1,8 @@
+import { Link } from "react-scroll";
+import Image from "next/image";
+import BackToTopIconLightMode from "../../public/static/images/back_to_top_icon_light_mode.svg";
+import BackToTopIconDarkMode from "../../public/static/images/back_to_top_icon_dark_mode.svg";
+
 export default function BackToTopButton() {
   return(
     <div></div>
