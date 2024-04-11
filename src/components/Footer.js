7a479ec11f086755a97691acf60d1022cd8b011f
yuritavares <yuriatp@gmail.com>
2024-04-11 14:38:13 -0300

    Adding documentation link.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,18 @@
+import Link from "next/link";
+
 export default function Footer({ darkMode }) {
   return (
-    <section className={(darkMode ? "bg-capx-dark-box-bg " : "bg-capx-light-box-bg ") + "w-full h-16"}></section>
+    <section className={(darkMode ? "bg-capx-dark-box-bg " : "bg-capx-light-box-bg ") + "w-full h-16"}>
+      <div className="">
+        <div className="">
+          <Link
+            href="/"
+            target="_blank"
+            className={(darkMode ? "text-capx-dark-link border-capx-dark-link " : "text-capx-light-link border-capx-light-link ")}>
+            Documentation
+          </Link>
+        </div>
+      </div>
+    </section>
   )
 }
