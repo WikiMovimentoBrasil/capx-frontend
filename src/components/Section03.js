1f501201ecea427d6b76cb53fbe1e6f942249d14
yuritavares <yuriatp@gmail.com>
2024-04-11 18:50:06 -0300

    Section03: Adding basic elements.

--- src/components/Section03.js
+++ src/components/Section03.js
@@ -1,5 +1,27 @@
 export default function Section03() {
   return (
-    <section id="section03" className="flex w-10/12 h-screen mx-auto my-auto"></section>
+    <section id="section03" className="flex w-full h-screen mx-auto my-auto">
+      <div>
+        <h2>Contact Us</h2>
+      </div>
+      <div>
+        <input
+          type="text"
+          placeholder="Name"
+        />
+        <input
+          type="text"
+          placeholder="E-mail"
+        />
+        <textarea
+          type="text"
+          rows="4"
+          placeholder="Message"
+        />
+      </div>
+      <div>
+        <button>Submit your message</button>
+      </div>
+    </section>
   )
 }
