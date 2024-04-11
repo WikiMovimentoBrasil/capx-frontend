2411f2b0b4d0da0ae117e063787f062f9eedce0f
yuritavares <yuriatp@gmail.com>
2024-04-11 13:27:35 -0300

    Adding light/dark mode.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,5 @@
-export default function Footer() {
+export default function Footer({ darkMode }) {
   return (
-    <section className="w-full h-16"></section>
+    <section className={(darkMode ? "bg-capx-dark-box-bg " : "bg-capx-light-box-bg ") + "w-full h-16"}></section>
   )
 }
