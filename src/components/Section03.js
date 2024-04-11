b3fa4815a32108d5bc034fdba152112ada2793f2
yuritavares <yuriatp@gmail.com>
2024-04-11 19:03:34 -0300

    Section03: Adding style to section title.

--- src/components/Section03.js
+++ src/components/Section03.js
@@ -1,26 +1,31 @@
 export default function Section03() {
   return (
-    <section id="section03" className="flex w-full h-screen mx-auto my-auto">
-      <div>
-        <h2>Contact Us</h2>
-      </div>
-      <div>
-        <input
-          type="text"
-          placeholder="Name"
-        />
-        <input
-          type="text"
-          placeholder="E-mail"
-        />
-        <textarea
-          type="text"
-          rows="4"
-          placeholder="Message"
-        />
-      </div>
-      <div>
-        <button>Submit your message</button>
+    <section id="section03" className="flex w-full h-fit my-24">
+      <div className="w-10/12 h-fit my-auto mx-auto">
+        <div>
+          <h2 className="font-extrabold text-2xl text-center mb-6">Contact Us</h2>
+        </div>
+        <div>
+          <input
+            type="text"
+            placeholder="Name"
+            className="w-full"
+          />
+          <input
+            type="text"
+            placeholder="E-mail"
+            className="w-full"
+          />
+          <textarea
+            type="text"
+            rows="4"
+            placeholder="Message"
+            className="w-full"
+          />
+        </div>
+        <div>
+          <button>Submit your message</button>
+        </div>
       </div>
     </section>
   )
