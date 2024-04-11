f49dbc98434438129d86b3dfdfb1c25a68e104a0
yuritavares <yuriatp@gmail.com>
2024-04-11 20:49:22 -0300

    BackToTopButton: Adding new script to component.

--- src/components/BackToTopButton.js
+++ src/components/BackToTopButton.js
@@ -1,18 +1,21 @@
-import { Link } from "react-scroll";
 import Image from "next/image";
+import Script from "next/script";
+import { Link } from "react-scroll";
+import BackToTopButtonScript from "../../public/static/script.js";
 import BackToTopIconLightMode from "../../public/static/images/back_to_top_icon_light_mode.svg";
 import BackToTopIconDarkMode from "../../public/static/images/back_to_top_icon_dark_mode.svg";
 
 export default function BackToTopButton({ darkMode }) {
   return (
     <Link
+      id="bbtbutton"
       activeClass="active"
       to="section01"
       spy={true}
       smooth={true}
       duration={500}
       delay={150}
-      className={(darkMode ? "bg-capx-dark-box-bg " : "bg-capx-light-box-bg ") + "fixed bottom-10 left-2 cursor-pointer z-50 rounded-full"}
+      className={(darkMode ? "bg-capx-dark-box-bg " : "bg-capx-light-box-bg ") + "hidden fixed bottom-10 left-2 cursor-pointer z-50 rounded-full"}
     >
       <Image
         priority={true}
@@ -20,6 +23,7 @@ export default function BackToTopButton({ darkMode }) {
         alt="Back to top button."
         className="max-w-12 p-1"
       />
+      <Script src={BackToTopButtonScript} />
     </Link>
   )
 }
