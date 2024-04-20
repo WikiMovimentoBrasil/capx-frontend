c8780557902e426afccc5f132c0fffe8af7afcf2
yuritavares <yuriatp@gmail.com>
2024-04-20 10:53:22 -0300

    api/login: first step.

--- src/pages/api/login.js
+++ src/pages/api/login.js
@@ -1,6 +1,13 @@
+import axios from "axios";
+
 export default async function startLogin(req, res) {
   if (req.method === "POST") {
-    res.status(200);
+    const startLoginResponse = await axios.post(process.env.START_LOGIN_URL);
+    if (startLoginResponse.data.oauth_callback_confirmed) {
+      res.status(200).json(startLoginResponse.data);
+    } else {
+      res.status(500);
+    }
   }
   else {
     res.status(405);
