ae41da5977582cd0b1da224ab09ba4c04b15668a
yuritavares <yuriatp@gmail.com>
2024-04-20 10:25:14 -0300

    LoginButton: Adding function to handle button click.

--- src/components/LoginButton.js
+++ src/components/LoginButton.js
@@ -4,8 +4,12 @@ import { useState } from "react";
 export default function LoginButton() {
   const [loginStarted, setLoginStarted] = useState(false);
 
+  const startLogin = async () => {
+    setLoginStarted(true);
+  }
+
   return (
-    <button className="flex w-fit h-fit my-auto bg-capx-secondary-purple text-[#F6F6F6] px-4 sm:px-5 py-2 rounded-full">
+    <button onClick={startLogin} className="flex w-fit h-fit my-auto bg-capx-secondary-purple text-[#F6F6F6] px-4 sm:px-5 py-2 rounded-full">
       {loginStarted ?
         <div className="animate-spin ease-linear h-6 w-6 rounded-full border-4 border-l-white border-r-white border-b-white border-t-capx-primary-green"></div>
         :
