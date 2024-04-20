46bf951e1e2400ce803b3aab83eaedd87f7f34af
yuritavares <yuriatp@gmail.com>
2024-04-20 10:21:55 -0300

    LoginButton: Adding loading animation in case login has started.

--- src/components/LoginButton.js
+++ src/components/LoginButton.js
@@ -1,5 +1,16 @@
+"use client";
+import { useState } from "react";
+
 export default function LoginButton() {
+  const [loginStarted, setLoginStarted] = useState(false);
+
   return (
-    <button className="flex w-fit h-fit my-auto bg-capx-secondary-purple text-white px-4 sm:px-5 py-2 rounded-full cursor-pointer">Login</button>
+    <button className="flex w-fit h-fit my-auto bg-capx-secondary-purple text-[#F6F6F6] px-4 sm:px-5 py-2 rounded-full">
+      {loginStarted ?
+        <div className="animate-spin ease-linear h-6 w-6 rounded-full border-4 border-l-white border-r-white border-b-white border-t-capx-primary-green"></div>
+        :
+        "Login"
+      }
+    </button>
   )
 }
