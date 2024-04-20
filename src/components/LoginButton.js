9d7d17a59c5135c536dbc0c5a4745a4943dee785
yuritavares <yuriatp@gmail.com>
2024-04-20 09:41:16 -0300

    LoginButton: Creating new component.

---
+++ src/components/LoginButton.js
@@ -0,0 +1,5 @@
+export default function LoginButton() {
+  return (
+    <button className="flex w-fit h-fit my-auto bg-capx-secondary-purple text-white px-4 sm:px-5 py-2 rounded-full cursor-pointer">Login</button>
+  )
+}
