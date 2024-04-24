ab03976ed8588ac4011c617caaa4f812e3479689
yuritavares <yuriatp@gmail.com>
2024-04-24 19:28:04 -0300

    Oauth: Adding CapX logo.

--- src/app/oauth/page.js
+++ src/app/oauth/page.js
@@ -1,5 +1,18 @@
+import Image from "next/image";
+import CapXLogo from "../../../public/static/images/capx_logo.svg";
+
 export default function OAuth() {
   return (
-    <section className="flex w-screen h-screen font-montserrat"></section>
+    <section className="flex w-screen h-screen font-montserrat">
+      <div className="flex flex-wrap w-1/2 mx-auto my-auto">
+        <div className="flex w-fit mx-auto mb-4">
+          <Image
+            src={CapXLogo}
+            alt="Capacity Exchange logo image."
+            className="w-16"
+          />
+        </div>
+      </div>
+    </section>
   )
 }
