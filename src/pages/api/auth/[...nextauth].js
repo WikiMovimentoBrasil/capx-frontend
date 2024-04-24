170f5d24825108fcd1926c9c6ddd26f9e792ef5f
yuritavares <yuriatp@gmail.com>
2024-04-24 18:52:05 -0300

    api/auth: Adding our own logic to Credentials Provider.

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -7,6 +7,13 @@ export const authOptions = {
     CredentialsProvider({
       name: 'Credentials',
       async authorize(credentials, request) {
+        // The logic of the last login step. Returns either an object representing
+        // a user or value that is false/null if the credentials are invalid.
+        const loginResponse = await axios.post(process.env.LOGIN_STEP03_URL, credentials);
+        if (loginResponse.status == 200) {
+          const user = { token: loginResponse.data.token }
+          return user
+        }
         // Return null if user data could not be retrieved
         return null
       }
