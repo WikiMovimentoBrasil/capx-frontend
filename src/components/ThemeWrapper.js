26801c7c5d69421e2822e29a0338f4107b008ccd
yuritavares <yuriatp@gmail.com>
2024-04-25 19:19:45 -0300

    ThemeWrapper: Moving page code to this component.

--- src/components/ThemeWrapper.js
+++ src/components/ThemeWrapper.js
@@ -1,5 +1,17 @@
+"use client";
+import { useState } from "react";
+import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
+import Section01 from "@/components/Section01";
+
 export default function ThemeWrapper(props) {
+  const [darkMode, setDarkMode] = useState(props.darkMode.value === "true");
+
   return (
-    <main className={"flex flex-wrap flex-col w-full font-montserrat"}></main>
+    <main className={(darkMode ? "bg-capx-dark-bg text-capx-light-bg " : "bg-capx-light-bg text-capx-dark-bg ") + " flex flex-wrap flex-col w-full font-montserrat"}>
+      <Navbar darkMode={darkMode} setDarkMode={setDarkMode}></Navbar>
+      <Section01></Section01>
+      <Footer darkMode={darkMode}></Footer>
+    </main>
   )
 }
