c27e24a7bccfdccc2c38f1d9a519b38f89d1a2fc
yuritavares <yuriatp@gmail.com>
2024-04-25 20:52:39 -0300

    ApplicationWrapper: Renaming ThemeWrapper component.

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,11 @@
 import { cookies } from "next/headers";
-import ThemeWrapper from "@/components/ThemeWrapper";
+import ApplicationWrapper from "@/components/ApplicationWrapper";
 
 export default function Home() {
   const cookieStore = cookies();
   const darkMode = cookieStore.get("dark_mode");
 
   return (
-    <ThemeWrapper darkMode={darkMode}></ThemeWrapper>
+    <ApplicationWrapper darkMode={darkMode}></ApplicationWrapper>
   );
 }
