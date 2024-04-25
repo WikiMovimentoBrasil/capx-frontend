773d5dcbdc8157bdd61563e6704ac243b77e393e
yuritavares <yuriatp@gmail.com>
2024-04-25 19:52:41 -0300

    Page use cookies and send color mode data to child component.

--- src/app/page.js
+++ src/app/page.js
@@ -1,22 +1,11 @@
-"use client";
-import Footer from "@/components/Footer";
-import Navbar from "@/components/Navbar";
-import Section01 from "@/components/Section01";
-import Section02 from "@/components/Section02";
-import Section03 from "@/components/Section03";
-import BackToTopButton from "@/components/BackToTopButton";
-import { useState } from "react";
+import { cookies } from "next/headers";
+import ThemeWrapper from "@/components/ThemeWrapper";
 
 export default function Home() {
-  const [darkMode, setDarkMode] = useState(false);
+  const cookieStore = cookies();
+  const darkMode = cookieStore.get("dark_mode");
 
   return (
-    <main className={(darkMode ? "bg-capx-dark-bg text-capx-light-bg " : "bg-capx-light-bg text-capx-dark-bg ") + " flex flex-wrap flex-col w-full font-montserrat"}>
-      <Navbar darkMode={darkMode} setDarkMode={setDarkMode}></Navbar>
-      <Section01></Section01>
-      <Section02 darkMode={darkMode}></Section02>
-      <Footer darkMode={darkMode}></Footer>
-      <BackToTopButton darkMode={darkMode} />
-    </main>
+    <ThemeWrapper darkMode={darkMode}></ThemeWrapper>
   );
 }
