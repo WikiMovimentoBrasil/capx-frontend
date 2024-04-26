539e72b10d9e296aca40c01ace662ba3b295a375
yuritavares <yuriatp@gmail.com>
2024-04-26 10:15:20 -0300

    Profile/MainWrapper: Using state to track dark mode.

--- src/app/profile/components/MainWrapper.js
+++ src/app/profile/components/MainWrapper.js
@@ -1,3 +1,8 @@
-export default function MainWrapper({props}) {
+"use client";
+import { useState } from "react";
+
+export default function MainWrapper(props) {
+  const [darkMode, setDarkMode] = useState(props.darkMode.value === "true");
+  
   return (<></>)
 }
