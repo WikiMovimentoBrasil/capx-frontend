1f96b95efa534d102109021785b3e3b6964c0fc8
yuritavares <yuriatp@gmail.com>
2024-04-26 10:34:37 -0300

    ApplicationWrapper: Redirecting user to /profile if logged in.

--- src/components/ApplicationWrapper.js
+++ src/components/ApplicationWrapper.js
@@ -1,14 +1,23 @@
 "use client";
 import { useState } from "react";
 import BaseWrapper from "./BaseWrapper";
+import { useRouter } from 'next/navigation';
 import Section01 from "@/components/Section01";
 
 export default function ApplicationWrapper(props) {
+  const router = useRouter();
   const [darkMode, setDarkMode] = useState(props.darkMode.value === "true");
 
-  return (
-    <BaseWrapper session={props.session} darkMode={darkMode} setDarkMode={setDarkMode}>
-      <Section01></Section01>
-    </BaseWrapper>
-  )
+  // Redirecting user to /profile if logged in
+  if (props.session) {
+    router.push("/profile");
+  }
+  // Rendering initial landing page
+  else {
+    return (
+      <BaseWrapper session={props.session} darkMode={darkMode} setDarkMode={setDarkMode}>
+        <Section01></Section01>
+      </BaseWrapper>
+    )
+  }
 }
