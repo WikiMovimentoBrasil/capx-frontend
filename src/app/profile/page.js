9f0a515e0e1da6cff01779fce1579f3fb181fd85
yuritavares <yuriatp@gmail.com>
2024-04-26 10:10:23 -0300

    Profile: Using cookies to get color theme.

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -1,7 +1,10 @@
+import { cookies } from "next/headers";
 import { getServerSession } from "next-auth/next";
 
 export default async function ProfilePage() {
+  const cookieStore = cookies();
   const session = await getServerSession();
+  const darkMode = cookieStore.get("dark_mode");
 
   return (
     <div></div>
