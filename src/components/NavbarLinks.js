e1016bfa80657e355fc819d6d5454765c01562d0
yuritavares <yuriatp@gmail.com>
2024-04-25 21:51:22 -0300

    NavbarLinks: Checkig if user is logged in to show specific links.

--- src/components/NavbarLinks.js
+++ src/components/NavbarLinks.js
@@ -1,6 +1,34 @@
+import NextLink from "next/link";
 import { Link } from "react-scroll";
 
 export default function NavbarLinks({ session }) {
+  // User is logged in
+  if (session) {
+    return (
+      <div className="flex space-x-12">
+        {/* 'Skills' button */}
+        <NextLink
+          href="/profile"
+          className="hidden sm:block flex my-auto cursor-pointer hover:border-b hover:border-current"
+        >
+          My Profile
+        </NextLink>
+        <NextLink
+          href="/skills"
+          className="hidden sm:block flex my-auto cursor-pointer hover:border-b hover:border-current"
+        >
+          Skills
+        </NextLink>
+        <NextLink
+          href="/events"
+          className="hidden sm:block flex my-auto cursor-pointer hover:border-b hover:border-current"
+        >
+          Events
+        </NextLink>
+      </div>
+    )
+  }
+
   return (
     <div className="flex space-x-12">
       {/* 'About' button */}
