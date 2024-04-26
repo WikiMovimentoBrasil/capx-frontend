e467870e1206c51fb1929e7c16feac5771a8726d
yuritavares <yuriatp@gmail.com>
2024-04-26 17:55:38 -0300

    ApplicationWrapper: Removing Router.

--- src/components/ApplicationWrapper.js
+++ src/components/ApplicationWrapper.js
@@ -1,26 +1,17 @@
 "use client";
 import { useState } from "react";
 import BaseWrapper from "./BaseWrapper";
-import { useRouter } from 'next/navigation';
 import Section01 from "@/components/Section01";
 import BlankSection from "./BlankSection";
 
 export default function ApplicationWrapper(props) {
-  const router = useRouter();
   const [darkMode, setDarkMode] = useState(props.darkMode.value === "true");
 
-  // Redirecting user to /profile if logged in
-  if (props.session) {
-    router.push("/profile");
-  }
-  // Rendering initial landing page
-  else {
-    return (
-      <BaseWrapper session={props.session} darkMode={darkMode} setDarkMode={setDarkMode}>
-        <Section01></Section01>
-      </BaseWrapper>
-    )
-  }
+  return (
+    <BaseWrapper session={props.session} darkMode={darkMode} setDarkMode={setDarkMode}>
+      <Section01></Section01>
+    </BaseWrapper>
+  )
   // Using a 'blank section' to maintain bg color
   // and avoid 'blink effect' in case of redirect
   return (
