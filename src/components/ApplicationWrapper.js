2ed016551ebbb9638b8b218e763491ddbd2d481b
yuritavares <yuriatp@gmail.com>
2024-04-26 17:56:17 -0300

    ApplicationWrapper: Removing BlankSection component.

--- src/components/ApplicationWrapper.js
+++ src/components/ApplicationWrapper.js
@@ -2,7 +2,6 @@
 import { useState } from "react";
 import BaseWrapper from "./BaseWrapper";
 import Section01 from "@/components/Section01";
-import BlankSection from "./BlankSection";
 
 export default function ApplicationWrapper(props) {
   const [darkMode, setDarkMode] = useState(props.darkMode.value === "true");
@@ -11,10 +10,5 @@ export default function ApplicationWrapper(props) {
     <BaseWrapper session={props.session} darkMode={darkMode} setDarkMode={setDarkMode}>
       <Section01></Section01>
     </BaseWrapper>
-  )
-  // Using a 'blank section' to maintain bg color
-  // and avoid 'blink effect' in case of redirect
-  return (
-    <BlankSection darkMode={darkMode} />
   )
 }
