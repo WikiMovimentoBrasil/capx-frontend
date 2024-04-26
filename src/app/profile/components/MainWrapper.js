86f210b41e44d5af89da34db568925cd1dbdff1c
yuritavares <yuriatp@gmail.com>
2024-04-26 10:36:47 -0300

    Profle/MainWrapper: Adding BaseWrapper component.

--- src/app/profile/components/MainWrapper.js
+++ src/app/profile/components/MainWrapper.js
@@ -1,8 +1,11 @@
 "use client";
 import { useState } from "react";
+import BaseWrapper from "@/components/BaseWrapper";
 
 export default function MainWrapper(props) {
   const [darkMode, setDarkMode] = useState(props.darkMode.value === "true");
-  
-  return (<></>)
+
+  return (
+    <BaseWrapper session={props.session} darkMode={darkMode} setDarkMode={setDarkMode}></BaseWrapper>
+  )
 }
