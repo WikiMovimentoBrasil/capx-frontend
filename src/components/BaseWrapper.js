4ceff68d71d3ecdb6bb80f014cfa6fe443a7e6de
yuritavares <yuriatp@gmail.com>
2024-04-29 20:47:46 -0300

    BaseWrapper: Avoiding scroll when mobile menu is open.

--- src/components/BaseWrapper.js
+++ src/components/BaseWrapper.js
@@ -3,7 +3,7 @@ import Footer from "@/components/Footer";
 
 export default function BaseWrapper({ children, session, darkMode, setDarkMode, mobileMenuStatus, setMobileMenuStatus }) {
   return (
-    <main className={(darkMode ? "bg-capx-dark-bg text-capx-light-bg " : "bg-capx-light-bg text-capx-dark-bg ") + " flex flex-wrap flex-col w-full font-montserrat"}>
+    <main className={(darkMode ? "bg-capx-dark-bg text-capx-light-bg " : "bg-capx-light-bg text-capx-dark-bg ") + (mobileMenuStatus ? "fixed " : "") + " flex flex-wrap flex-col w-full font-montserrat"}>
       <Navbar session={session} darkMode={darkMode} setDarkMode={setDarkMode} mobileMenuStatus={mobileMenuStatus} setMobileMenuStatus={setMobileMenuStatus}></Navbar>
       {children}
       <Footer darkMode={darkMode}></Footer>
