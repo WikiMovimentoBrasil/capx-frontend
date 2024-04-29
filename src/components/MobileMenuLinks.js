705d969e972bbcfed105725fd6c91c042411db5e
yuritavares <yuriatp@gmail.com>
2024-04-29 16:28:43 -0300

    MobileMenuLinks: Adding params to react scroll Link.

--- src/components/MobileMenuLinks.js
+++ src/components/MobileMenuLinks.js
@@ -1,7 +1,7 @@
 import NextLink from "next/link";
 import { Link } from "react-scroll";
 
-export default function MobileMenuLinks({ session }) {
+export default function MobileMenuLinks({ session, handleMenuStatus }) {
   const menuDataLoggedIn = [
     { title: "My Profile", to: "/profile" },
     { title: "Skills", to: "/skills" },
@@ -32,16 +32,27 @@ export default function MobileMenuLinks({ session }) {
 
   return (
     <div className="flex flex-wrap w-10/12 text-2xl mx-auto space-y-4">
-    {menuDataNotLoggedIn.map((item, index) => {
-      return (
-        <Link
-          href={item.to}
-          className="w-full cursor-pointer border-b py-3"
-        >
-          {item.title}
-        </Link>
-      )
-    })}
-  </div>
+      {menuDataNotLoggedIn.map((item, index) => {
+        return (
+          <Link
+            activeClass="active"
+            to={item.to}
+            spy={true}
+            smooth={true}
+            hashSpy={true}
+            duration={500}
+            delay={150}
+            isDynamic={true}
+            ignoreCancelEvents={false}
+            spyThrottle={500}
+            onClick={handleMenuStatus}
+            key={"menuLink" + index.toString()}
+            className="w-full cursor-pointer border-b py-3"
+          >
+            {item.title}
+          </Link>
+        )
+      })}
+    </div>
   )
 }
