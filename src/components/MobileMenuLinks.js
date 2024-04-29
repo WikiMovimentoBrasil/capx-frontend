48a948b15751b314fc26a87c2d0c7049ef407d1f
yuritavares <yuriatp@gmail.com>
2024-04-29 15:52:27 -0300

    MobileMenuLinks: Rendering menu links when user is not logged in.

--- src/components/MobileMenuLinks.js
+++ src/components/MobileMenuLinks.js
@@ -1,10 +1,15 @@
 import NextLink from "next/link";
+import { Link } from "react-scroll";
 
 export default function MobileMenuLinks({ session }) {
   const menuDataLoggedIn = [
     { title: "My Profile", to: "/profile" },
     { title: "Skills", to: "/skills" },
     { title: "Events", to: "/events" },
+  ];
+
+  const menuDataNotLoggedIn = [
+    { title: "About", to: "section02" },
   ];
 
   // User is logged in
@@ -24,4 +29,19 @@ export default function MobileMenuLinks({ session }) {
       </div>
     )
   }
+
+  return (
+    <div className="flex flex-wrap w-10/12 text-2xl mx-auto space-y-4">
+    {menuDataNotLoggedIn.map((item, index) => {
+      return (
+        <Link
+          href={item.to}
+          className="w-full cursor-pointer border-b py-3"
+        >
+          {item.title}
+        </Link>
+      )
+    })}
+  </div>
+  )
 }
