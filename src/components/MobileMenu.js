8fe390d170b814b12063ad5c14e229d6fdaf2cd3
yuritavares <yuriatp@gmail.com>
2024-04-29 13:15:12 -0300

    MobileMenu: Adding a function to handle menu status.

--- src/components/MobileMenu.js
+++ src/components/MobileMenu.js
@@ -1,15 +1,22 @@
+"use client";
 import Image from "next/image";
 import IconCloseMobileMenu from "../../public/static/images/icon_close_mobile_menu.svg";
 
 export default function MobileMenu(props) {
+  const handleMenuStatus = () => {
+    props.setMobileMenuStatus((prevState) => !prevState);
+  };
+
   return (
-    <div className="fixed w-screen h-screen pb-10 origin-top-right z-50">
-      <div className="flex flex-row-reverse w-10/12 mx-auto mt-6 mb-8">
-        <Image
-          src={IconCloseMobileMenu}
-          alt="Button to close menu."
-          className="fill-current h-10 w-10 text-gray-700"
-        />
+    <div className="fixed w-screen h-screen bg-gray-100 pb-10 origin-top-right z-50">
+      <div className="flex flex-row-reverse w-10/12 mx-auto mt-6 mb-8 cursor-pointer">
+        <a onClick={handleMenuStatus}>
+          <Image
+            src={IconCloseMobileMenu}
+            alt="Button to close menu."
+            className="fill-current h-10 w-10 text-gray-700"
+          />
+        </a>
       </div>
     </div>
   )
