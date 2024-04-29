7e093a2ac6699cf1687c49857dad5d9c58f64449
yuritavares <yuriatp@gmail.com>
2024-04-29 16:07:13 -0300

    MobileMenu: Adding dark/light mode logic.

--- src/components/MobileMenu.js
+++ src/components/MobileMenu.js
@@ -1,19 +1,20 @@
 "use client";
 import Image from "next/image";
-import IconCloseMobileMenu from "../../public/static/images/icon_close_mobile_menu.svg";
+import IconCloseMobileMenuLightMode from "../../public/static/images/close_mobile_menu_icon_light_mode.svg";
+import IconCloseMobileMenuDarkMode from "../../public/static/images/close_mobile_menu_icon_dark_mode.svg";
 import MobileMenuLinks from "@/components/MobileMenuLinks";
 
-export default function MobileMenu({session, setMobileMenuStatus}) {
+export default function MobileMenu({session, setMobileMenuStatus, darkMode}) {
   const handleMenuStatus = () => {
     setMobileMenuStatus((prevState) => !prevState);
   };
 
   return (
-    <div className="fixed w-screen h-screen bg-gray-100 pb-10 origin-top-right z-50">
+    <div className={(darkMode ? "bg-capx-dark-box-bg text-capx-light-bg " : "bg-capx-light-box-bg text-capx-dark-bg ") + "fixed w-screen h-screen pb-10 origin-top-right z-50"}>
       <div className="flex flex-row-reverse w-10/12 mx-auto mt-6 mb-8">
         <a onClick={handleMenuStatus}>
           <Image
-            src={IconCloseMobileMenu}
+            src={darkMode ? IconCloseMobileMenuDarkMode : IconCloseMobileMenuLightMode}
             alt="Button to close menu."
             className="fill-current h-10 w-10 text-gray-700 cursor-pointer"
           />
