12e51523f0b84fc266ab47cefae5304af75f29bd
yuritavares <yuriatp@gmail.com>
2024-04-29 15:55:19 -0300

    MobileMenu: Adding MobileMenuLinks component.

--- src/components/MobileMenu.js
+++ src/components/MobileMenu.js
@@ -1,23 +1,25 @@
 "use client";
 import Image from "next/image";
 import IconCloseMobileMenu from "../../public/static/images/icon_close_mobile_menu.svg";
+import MobileMenuLinks from "@/components/MobileMenuLinks";
 
-export default function MobileMenu(props) {
+export default function MobileMenu({session, setMobileMenuStatus}) {
   const handleMenuStatus = () => {
-    props.setMobileMenuStatus((prevState) => !prevState);
+    setMobileMenuStatus((prevState) => !prevState);
   };
 
   return (
     <div className="fixed w-screen h-screen bg-gray-100 pb-10 origin-top-right z-50">
-      <div className="flex flex-row-reverse w-10/12 mx-auto mt-6 mb-8 cursor-pointer">
+      <div className="flex flex-row-reverse w-10/12 mx-auto mt-6 mb-8">
         <a onClick={handleMenuStatus}>
           <Image
             src={IconCloseMobileMenu}
             alt="Button to close menu."
-            className="fill-current h-10 w-10 text-gray-700"
+            className="fill-current h-10 w-10 text-gray-700 cursor-pointer"
           />
         </a>
       </div>
+      <MobileMenuLinks session={session} />
     </div>
   )
 }
