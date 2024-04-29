a1ed108b174acf1eeadf1b3f0e1049f1c90886bf
yuritavares <yuriatp@gmail.com>
2024-04-29 17:01:16 -0300

    Creating state to check mobile menu status.

--- src/app/profile/components/MainWrapper.js
+++ src/app/profile/components/MainWrapper.js
@@ -4,8 +4,15 @@ import BaseWrapper from "@/components/BaseWrapper";
 
 export default function MainWrapper(props) {
   const [darkMode, setDarkMode] = useState(props.darkMode.value === "true");
+  const [mobileMenuStatus, setMobileMenuStatus] = useState(false);
 
   return (
-    <BaseWrapper session={props.session} darkMode={darkMode} setDarkMode={setDarkMode}></BaseWrapper>
+    <BaseWrapper
+      session={props.session}
+      darkMode={darkMode}
+      setDarkMode={setDarkMode}
+      mobileMenuStatus={mobileMenuStatus}
+      setMobileMenuStatus={setMobileMenuStatus}
+    ></BaseWrapper>
   )
 }
--- src/components/ApplicationWrapper.js
+++ src/components/ApplicationWrapper.js
@@ -5,9 +5,16 @@ import Section01 from "@/components/Section01";
 
 export default function ApplicationWrapper(props) {
   const [darkMode, setDarkMode] = useState(props.darkMode.value === "true");
+  const [mobileMenuStatus, setMobileMenuStatus] = useState(false);
 
   return (
-    <BaseWrapper session={props.session} darkMode={darkMode} setDarkMode={setDarkMode}>
+    <BaseWrapper
+      session={props.session}
+      darkMode={darkMode}
+      setDarkMode={setDarkMode}
+      mobileMenuStatus={mobileMenuStatus}
+      setMobileMenuStatus={setMobileMenuStatus}
+    >
       <Section01></Section01>
     </BaseWrapper>
   )
