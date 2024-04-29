f6fa6f8ffbe05e9b11e85b85cb257b6e2c928434
yuritavares <yuriatp@gmail.com>
2024-04-29 17:10:28 -0300

    Navbar: Adding animated MobileMenu.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,38 +1,47 @@
 "use client";
 import Image from 'next/image';
 import NextLink from "next/link";
+import MobileMenu from "./MobileMenu";
 import NavbarLinks from "./NavbarLinks";
 import NavbarConfig from "./NavbarConfig";
 import SignInButton from "./SignInButton";
 import SignOutButton from "./SignOutButton";
+import { AnimatePresence } from "framer-motion";
 import CapXLogo from "../../public/static/images/capx_logo.svg";
-import MobileMenuLightMode from "../../public/static/images/mobile_menu_light_mode.svg";
-import MobileMenuDarkMode from "../../public/static/images/mobile_menu_dark_mode.svg";
-import LanguageMenuLightMode from "../../public/static/images/lang_light_mode_icon.svg";
-import LanguageMenuDarkMode from "../../public/static/images/lang_dark_mode_icon.svg";
 
-export default function Navbar({ session, darkMode, setDarkMode }) {
+export default function Navbar({ session, darkMode, setDarkMode, mobileMenuStatus, setMobileMenuStatus }) {
   return (
-    <nav className={(darkMode ? "bg-capx-dark-box-bg " : "bg-capx-light-box-bg ") + "fixed w-full h-16 m-auto left-0 right-0 z-50"}>
-      <div className="flex w-full h-full place-content-between">
-        {/* Capacity Exchange Logo */}
-        <div className="relative w-24">
-          <div className={(darkMode ? "bg-capx-dark-box-bg " : "bg-capx-light-box-bg ") + "flex w-fit h-fit rounded-full p-4"}>
-            <NextLink href="/">
-              <Image
-                priority={true}
-                src={CapXLogo}
-                alt="Capacity Exchange logo image."
-              />
-            </NextLink>
+    <>
+      <AnimatePresence>
+        {mobileMenuStatus &&
+          <MobileMenu
+            session={session}
+            darkMode={darkMode}
+            mobileMenuStatus={mobileMenuStatus}
+            setMobileMenuStatus={setMobileMenuStatus} />
+        }
+      </AnimatePresence>
+      <nav className={(darkMode ? "bg-capx-dark-box-bg " : "bg-capx-light-box-bg ") + "fixed w-full h-16 m-auto left-0 right-0 z-40"}>
+        <div className="flex w-full h-full place-content-between">
+          {/* Capacity Exchange Logo */}
+          <div className="relative w-24">
+            <div className={(darkMode ? "bg-capx-dark-box-bg " : "bg-capx-light-box-bg ") + "flex w-fit h-fit rounded-full p-4"}>
+              <NextLink href="/">
+                <Image
+                  priority={true}
+                  src={CapXLogo}
+                  alt="Capacity Exchange logo image."
+                />
+              </NextLink>
+            </div>
+          </div>
+          <div className="flex h-full mr-6 space-x-12 sm:space-x-14">
+            <NavbarLinks session={session} />
+            <NavbarConfig darkMode={darkMode} setDarkMode={setDarkMode} setMobileMenuStatus={setMobileMenuStatus} />
+            {session ? (<SignOutButton></SignOutButton>) : (<SignInButton></SignInButton>)}
           </div>
         </div>
-        <div className="flex h-full mr-6 space-x-12 sm:space-x-14">
-          <NavbarLinks session={session} />
-          <NavbarConfig darkMode={darkMode} setDarkMode={setDarkMode} />
-          {session ? (<SignOutButton></SignOutButton>) : (<SignInButton></SignInButton>)}          
-        </div>
-      </div>
-    </nav>
+      </nav>
+    </>
   );
 }
