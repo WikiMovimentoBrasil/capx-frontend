103b7609ea93b9d220fa7dcc64f01f5d8a9f56a3
yuritavares <yuriatp@gmail.com>
2024-04-29 15:45:06 -0300

    MobileMenuLinks: Rendering menu links when user is logged in.

--- src/components/MobileMenuLinks.js
+++ src/components/MobileMenuLinks.js
@@ -1,8 +1,27 @@
+import NextLink from "next/link";
+
 export default function MobileMenuLinks({ session }) {
-  const menuData = [
+  const menuDataLoggedIn = [
     { title: "My Profile", to: "/profile" },
     { title: "Skills", to: "/skills" },
     { title: "Events", to: "/events" },
   ];
-  return (<div></div>)
+
+  // User is logged in
+  if (session) {
+    return (
+      <div className="flex flex-wrap w-10/12 text-2xl mx-auto space-y-4">
+        {menuDataLoggedIn.map((item, index) => {
+          return (
+            <NextLink
+              href={item.to}
+              className="w-full cursor-pointer border-b py-3"
+            >
+              {item.title}
+            </NextLink>
+          )
+        })}
+      </div>
+    )
+  }
 }
