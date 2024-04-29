4275635594e5c7d88dc5aa146d4402552f2f0674
yuritavares <yuriatp@gmail.com>
2024-04-29 15:31:29 -0300

    MobileMenuLinks: Adding menu links.

--- src/components/MobileMenuLinks.js
+++ src/components/MobileMenuLinks.js
@@ -1,3 +1,8 @@
 export default function MobileMenuLinks({ session }) {
+  const menuData = [
+    { title: "My Profile", to: "/profile" },
+    { title: "Skills", to: "/skills" },
+    { title: "Events", to: "/events" },
+  ];
   return (<div></div>)
 }
