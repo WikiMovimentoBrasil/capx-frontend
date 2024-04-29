e90e7191afba96fe457f8608b058c0d80463a64a
yuritavares <yuriatp@gmail.com>
2024-04-29 12:53:03 -0300

    MobileMenu: Adding button to close menu.

--- src/components/MobileMenu.js
+++ src/components/MobileMenu.js
@@ -1,3 +1,16 @@
+import Image from "next/image";
+import IconCloseMobileMenu from "../../public/static/images/icon_close_mobile_menu.svg";
+
 export default function MobileMenu(props) {
-  return (<div></div>)
+  return (
+    <div className="fixed w-screen h-screen pb-10 origin-top-right z-50">
+      <div className="flex flex-row-reverse w-10/12 mx-auto mt-6 mb-8">
+        <Image
+          src={IconCloseMobileMenu}
+          alt="Button to close menu."
+          className="fill-current h-10 w-10 text-gray-700"
+        />
+      </div>
+    </div>
+  )
 }
