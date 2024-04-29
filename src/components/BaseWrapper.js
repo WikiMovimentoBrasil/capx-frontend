7d723e29b9246c71452bb661a9f9911cb45cf191
yuritavares <yuriatp@gmail.com>
2024-04-29 17:02:47 -0300

    BaseWrapper: Passing mobileMenuStatus state to Navbar component.

--- src/components/BaseWrapper.js
+++ src/components/BaseWrapper.js
@@ -1,10 +1,10 @@
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 
-export default function BaseWrapper({ children, session, darkMode, setDarkMode }) {
+export default function BaseWrapper({ children, session, darkMode, setDarkMode, mobileMenuStatus, setMobileMenuStatus }) {
   return (
     <main className={(darkMode ? "bg-capx-dark-bg text-capx-light-bg " : "bg-capx-light-bg text-capx-dark-bg ") + " flex flex-wrap flex-col w-full font-montserrat"}>
-      <Navbar session={session} darkMode={darkMode} setDarkMode={setDarkMode}></Navbar>
+      <Navbar session={session} darkMode={darkMode} setDarkMode={setDarkMode} mobileMenuStatus={mobileMenuStatus} setMobileMenuStatus={setMobileMenuStatus}></Navbar>
       {children}
       <Footer darkMode={darkMode}></Footer>
     </main>
