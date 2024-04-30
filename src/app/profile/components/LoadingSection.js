ae2d5ea87377c57715b18e6f07babcb7c6c35845
yuritavares <yuriatp@gmail.com>
2024-04-30 10:44:55 -0300

    Profile/LoadingSection: Adding loading spinner.

--- src/app/profile/components/LoadingSection.js
+++ src/app/profile/components/LoadingSection.js
@@ -1,3 +1,7 @@
 export default function LoadingSection() {
-  return (<div></div>)
+  return (
+    <section className="grid w-full h-screen place-content-center">
+      <div className="animate-spin ease-linear h-10 w-10 rounded-full border-8 border-l-white border-r-white border-b-white border-t-capx-primary-green"></div>
+    </section>
+  )
 }
