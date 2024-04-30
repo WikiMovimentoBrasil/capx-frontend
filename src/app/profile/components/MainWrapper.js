5d9504471c99a7394690c710f4384dd7ad325955
yuritavares <yuriatp@gmail.com>
2024-04-30 10:46:03 -0300

    Profile/MainWrapper: Checking user session (client side).

--- src/app/profile/components/MainWrapper.js
+++ src/app/profile/components/MainWrapper.js
@@ -1,10 +1,17 @@
 "use client";
 import { useState } from "react";
+import { useSession } from "next-auth/react";
 import BaseWrapper from "@/components/BaseWrapper";
 
 export default function MainWrapper(props) {
+  let pageContent;
+  const { status, data } = useSession();
   const [darkMode, setDarkMode] = useState(props.darkMode.value === "true");
   const [mobileMenuStatus, setMobileMenuStatus] = useState(false);
+
+  if (status === "loading") {
+    pageContent = (<div></div>)
+  }
 
   return (
     <BaseWrapper
@@ -13,6 +20,8 @@ export default function MainWrapper(props) {
       setDarkMode={setDarkMode}
       mobileMenuStatus={mobileMenuStatus}
       setMobileMenuStatus={setMobileMenuStatus}
-    ></BaseWrapper>
+    >
+      {pageContent}
+    </BaseWrapper>
   )
 }
