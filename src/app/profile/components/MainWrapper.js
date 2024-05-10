13a3b7fbcf7db5000fc40dc812586205df243ffe
yuritavares <yuriatp@gmail.com>
2024-05-10 12:01:19 -0300

    Profile/MainWrapper: Requesting user data.

--- src/app/profile/components/MainWrapper.js
+++ src/app/profile/components/MainWrapper.js
@@ -1,14 +1,31 @@
 "use client";
-import { useState } from "react";
+import axios from "axios";
+import { useState, useEffect } from "react";
 import { useSession } from "next-auth/react";
-import BaseWrapper from "@/components/BaseWrapper";
 import LoadingSection from "./LoadingSection";
+import BaseWrapper from "@/components/BaseWrapper";
 
 export default function MainWrapper(props) {
   let pageContent;
   const { status, data } = useSession();
   const [darkMode, setDarkMode] = useState(props.darkMode.value === "true");
   const [mobileMenuStatus, setMobileMenuStatus] = useState(false);
+
+  useEffect(() => {
+    if (status == "authenticated") {
+      async function getUserData() {
+        const queryResponse = await axios.get("/api/profile", {
+          params: {
+            userId: data.user.id,
+          },
+          headers: {
+            'Authorization': `Token ${data.user.token}`,
+          }
+        });
+      }
+      getUserData();
+    }
+  }, [status]);
 
   if (status === "loading") {
     pageContent = (<LoadingSection darkMode={darkMode} />)
