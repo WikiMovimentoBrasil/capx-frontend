83a16784bb3ecfa1844678e992de573501f23416
yuritavares <yuriatp@gmail.com>
2024-05-13 19:17:38 -0300

    Profile/Edit/EditProfileForm: Fetching current user data to load inside form inputs.

--- src/app/profile/edit/components/EditProfileForm.js
+++ src/app/profile/edit/components/EditProfileForm.js
@@ -1,15 +1,34 @@
 "use client";
 import axios from "axios";
+import { useState, useEffect } from "react";
 import { useSession } from "next-auth/react";
 
 export default function EditProfileForm(props) {
   const { status, data } = useSession();
+  const [userData, setUserData] = useState({});
+
+  useEffect(() => {
+    if (status == "authenticated") {
+      async function getUserData() {
+        const queryResponse = await axios.get("/api/profile", {
+          params: {
+            userId: data.user.id,
+          },
+          headers: {
+            'Authorization': `Token ${data.user.token}`,
+          }
+        });
+        setUserData(queryResponse.data);
+      }
+      getUserData();
+    }
+  }, [status]);
 
   const handleSubmit = async (e) => {
     if (status == "authenticated") {
       e.preventDefault();
       const queryResponse = await axios.post("/api/profile",
-        {},
+        userData,
         {
           headers: {
             'Authorization': `Token ${data.user.token}`,
