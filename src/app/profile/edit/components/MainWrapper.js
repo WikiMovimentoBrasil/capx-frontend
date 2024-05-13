ca05e64ea5b2371969ca3b57c87cb3507d832966
yuritavares <yuriatp@gmail.com>
2024-05-13 16:39:04 -0300

    Profile/Edit/MainWrapper: Adding components.

--- src/app/profile/edit/components/MainWrapper.js
+++ src/app/profile/edit/components/MainWrapper.js
@@ -1,9 +1,20 @@
 "use client";
 import { useState } from "react";
+import BaseWrapper from "@/components/BaseWrapper";
 
 export default function MainWrapper(props) {
   const [darkMode, setDarkMode] = useState(props.darkMode.value === "true");
   const [mobileMenuStatus, setMobileMenuStatus] = useState(false);
 
-  return (<></>)
+  return (
+    <BaseWrapper
+      session={props.session}
+      darkMode={darkMode}
+      setDarkMode={setDarkMode}
+      mobileMenuStatus={mobileMenuStatus}
+      setMobileMenuStatus={setMobileMenuStatus}
+    >
+      {props.children}
+    </BaseWrapper>
+  )
 }
