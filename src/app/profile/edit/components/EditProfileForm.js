2826a6b9c14ca4386257cf2db3deb3ae44d2fa65
yuritavares <yuriatp@gmail.com>
2024-05-13 16:41:36 -0300

    Profile/Edit/EditProfileForm: Creating basic form.

--- src/app/profile/edit/components/EditProfileForm.js
+++ src/app/profile/edit/components/EditProfileForm.js
@@ -1,5 +1,20 @@
+"use client";
+import { useSession } from "next-auth/react";
+
 export default function EditProfileForm(props) {
+  const { status, data } = useSession();
+
+  const handleSubmit = async (e) => {
+    if (status == "authenticated") {
+      e.preventDefault();
+    }
+  };
+
   return (
-    <section className={"flex flex-wrap flex-col w-10/12 h-fit mx-auto place-content-start py-32"}></section>
+    <section className={"flex flex-wrap flex-col w-10/12 h-fit mx-auto place-content-start py-32"}>
+      <form onSubmit={handleSubmit}>
+        <button type="submit">Update</button>
+      </form>
+    </section>
   )
 }
