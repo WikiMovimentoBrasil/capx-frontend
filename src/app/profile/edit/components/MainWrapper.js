e0de94426baa274ef90962dbb6a233a8387c85c5
yuritavares <yuriatp@gmail.com>
2024-05-13 16:38:38 -0300

    Profile/Edit/MainWrapper: Creating states.

--- src/app/profile/edit/components/MainWrapper.js
+++ src/app/profile/edit/components/MainWrapper.js
@@ -1,3 +1,9 @@
+"use client";
+import { useState } from "react";
+
 export default function MainWrapper(props) {
+  const [darkMode, setDarkMode] = useState(props.darkMode.value === "true");
+  const [mobileMenuStatus, setMobileMenuStatus] = useState(false);
+
   return (<></>)
 }
