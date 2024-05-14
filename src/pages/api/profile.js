894a4adba8125d635abc67ae01cba087c47a0a1d
yuritavares <yuriatp@gmail.com>
2024-05-14 00:48:35 -0300

    api/profile: Adding OPTIONS method.

--- src/pages/api/profile.js
+++ src/pages/api/profile.js
@@ -9,21 +9,27 @@ export default async function userProfile(req, res) {
     });
     res.status(200).json(queryResponse.data);
   }
-  else {
-    if (req.method === "POST") {
-      const postBody = req.body;
-      const userId = postBody.user.id;
+  else if (req.method === "POST") {
+    const postBody = req.body;
+    const userId = postBody.user.id;
 
-      const queryResponse = await axios.put(process.env.PROFILE_URL + "/" + userId + "/", postBody, {
-        headers: {
-          'Authorization': req.headers.authorization
-        }
-      })
-      res.status(200).json(queryResponse.data);
-    }
-    else {
-      res.status(405);
-    }
-    res.end();
+    const queryResponse = await axios.put(process.env.PROFILE_URL + "/" + userId + "/", postBody, {
+      headers: {
+        'Authorization': req.headers.authorization
+      }
+    })
+    res.status(200).json(queryResponse.data);
   }
+  else if (req.method === "OPTIONS") {
+    const queryResponse = await axios.options(process.env.PROFILE_URL + "/" + req.query.userId, {
+      headers: {
+        'Authorization': req.headers.authorization
+      }
+    });
+    res.status(200).json(queryResponse.data.actions.PUT);
+  }
+  else {
+    res.status(405);
+  }
+  res.end();
 }
