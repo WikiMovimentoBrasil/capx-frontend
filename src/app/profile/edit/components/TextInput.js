97729e892d3d38f8661feeb04a52846a93118ef3
yuritavares <yuriatp@gmail.com>
2024-05-14 21:11:58 -0300

    Profile/Edit/TextInput: Adding component.

---
+++ src/app/profile/edit/components/TextInput.js
@@ -0,0 +1,16 @@
+export default function TextInput({ children, id, data, placeholder, onChange, type = "text" }) {
+  return (
+    <div className="w-full space-y-2 mb-8">
+      <label className="w-full ml-2">{children}</label>
+      <input
+        type={type}
+        placeholder={placeholder}
+        className="w-full h-12 pl-4 border-2 rounded-md"
+        id={id}
+        name={id}
+        value={data}
+        onChange={onChange}
+      />
+    </div >
+  )
+}
