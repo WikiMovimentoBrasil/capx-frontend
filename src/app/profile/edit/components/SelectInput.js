9f3d857fae51bb7ce67008227e3a0d95c7e3a502
yuritavares <yuriatp@gmail.com>
2024-05-14 21:59:33 -0300

    Profile/Edit/SelectInput: Adding basic style.

--- src/app/profile/edit/components/SelectInput.js
+++ src/app/profile/edit/components/SelectInput.js
@@ -1,6 +1,24 @@
 "use client";
 import Select from "react-select";
 
+const selectStyle = {
+  control: (base, state) => ({
+    ...base,
+    height: '48px',
+    paddingLeft: '4px',
+    color: 'rgb(30 41 59)',
+    border: state.isFocused ? 0 : 0,
+    // This line disable the blue border
+    boxShadow: state.isFocused ? 0 : 0,
+    "&:hover": {
+      border: state.isFocused ? 0 : 0
+    },
+  }),
+  placeholder: (styles) => ({
+    ...styles,
+  }),
+};
+
 export default function SelectInput({ id, data, defaultValue, onChange, children }) {
   return (
     <div className="w-full space-y-2 mb-8">
@@ -8,6 +26,7 @@ export default function SelectInput({ id, data, defaultValue, onChange, children
       <Select
         name={id}
         isSearchable={true}
+        styles={selectStyle}
         placeholder={""}
         className="border-2 rounded-md"
         options={data}
