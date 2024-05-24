67749034318346b68e1636a37cf4c5991ba4c014
yuritavares <yuriatp@gmail.com>
2024-05-24 00:53:29 -0300

    Profile/UserProfile: Adding EditProfileButton component.

--- src/app/profile/components/UserProfile.js
+++ src/app/profile/components/UserProfile.js
@@ -1,5 +1,11 @@
+import EditProfileButton from "./EditProfileButton";
+
 export default function UserProfile({ darkMode, userData }) {
   return (
-    <section className={"w-10/12 mx-auto py-32 place-content-start"}></section>
+    <section className={"w-10/12 mx-auto py-32 place-content-start"}>
+      <div className="w-full text-center mb-8 place-content-center">
+        <EditProfileButton />
+      </div>
+    </section>
   )
 }
