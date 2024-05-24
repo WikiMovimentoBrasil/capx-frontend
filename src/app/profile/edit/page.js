becf081c516e94eff39478f62e1900515bdbb3ae
yuritavares <yuriatp@gmail.com>
2024-05-24 01:04:18 -0300

    Profile/Edit: Loading dark mode data.

--- src/app/profile/edit/page.js
+++ src/app/profile/edit/page.js
@@ -4,4 +4,5 @@ import { getServerSession } from "next-auth/next";
 export default async function ProfilePage() {
   const cookieStore = cookies();
   const session = await getServerSession();
+  const darkMode = cookieStore.get("dark_mode") === undefined ? "false" : cookieStore.get("dark_mode");
 }
