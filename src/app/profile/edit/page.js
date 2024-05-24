3f7c6bcdcaaa308b87dd4ef26dbdd71f6ca3aea9
yuritavares <yuriatp@gmail.com>
2024-05-24 01:05:00 -0300

    Profile/Edit: Rendering components.

--- src/app/profile/edit/page.js
+++ src/app/profile/edit/page.js
@@ -1,8 +1,18 @@
 import { cookies } from "next/headers";
 import { getServerSession } from "next-auth/next";
+import MainWrapper from "./components/MainWrapper";
+import EditProfileForm from "./components/EditProfileForm";
 
 export default async function ProfilePage() {
   const cookieStore = cookies();
   const session = await getServerSession();
   const darkMode = cookieStore.get("dark_mode") === undefined ? "false" : cookieStore.get("dark_mode");
+
+  if (session) {
+    return (
+      <MainWrapper session={session !== null} darkMode={darkMode}>
+        <EditProfileForm session={session !== null}></EditProfileForm>
+      </MainWrapper>
+    )
+  }
 }
