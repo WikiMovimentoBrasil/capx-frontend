3cec0049e5800b41b22b9a88900bf143d7c642f7
yuritavares <yuriatp@gmail.com>
2024-05-24 01:03:45 -0300

    Profile/Edit: Loading session data.

--- src/app/profile/edit/page.js
+++ src/app/profile/edit/page.js
@@ -1,5 +1,7 @@
 import { cookies } from "next/headers";
+import { getServerSession } from "next-auth/next";
 
 export default async function ProfilePage() {
   const cookieStore = cookies();
+  const session = await getServerSession();
 }
