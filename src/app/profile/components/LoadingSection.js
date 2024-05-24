8a76e0b263d6cfd28c39b3e4234494f77108293d
yuritavares <yuriatp@gmail.com>
2024-05-24 00:32:38 -0300

    Profile/LoadingSection: Adding message to component.

--- src/app/profile/components/LoadingSection.js
+++ src/app/profile/components/LoadingSection.js
@@ -1,7 +1,10 @@
-export default function LoadingSection({ darkMode }) {
+export default function LoadingSection({ darkMode, message }) {
   return (
-    <section className="grid w-full h-screen place-content-center">
-      <div className={(darkMode ? "border-l-white border-r-white border-b-white border-t-capx-primary-green " : "border-l-capx-light-box-bg border-r-capx-light-box-bg border-b-capx-light-box-bg border-t-capx-secondary-purple ") + "animate-spin ease-linear h-10 w-10 rounded-full border-8"}></div>
+    <section className="grid w-full h-screen place-content-center text-center">
+      <div className="space-y-4">
+        <div className={(darkMode ? "border-l-white border-r-white border-b-white border-t-capx-primary-green " : "border-l-capx-light-box-bg border-r-capx-light-box-bg border-b-capx-light-box-bg border-t-capx-secondary-purple ") + "animate-spin ease-linear h-10 w-10 rounded-full border-8 mx-auto"}></div>
+        <h2 className="text-sm">LOADING {message}, PLEASE WAIT</h2>
+      </div>
     </section>
   )
 }
