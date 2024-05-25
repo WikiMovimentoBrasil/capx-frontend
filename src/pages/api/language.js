9f312f4eab565ecf6480df684da6f043106f4cd1
yuritavares <yuriatp@gmail.com>
2024-05-25 20:28:05 -0300

    api/language: Loading data according to query param.

--- src/pages/api/language.js
+++ src/pages/api/language.js
@@ -1,6 +1,11 @@
+import fs from 'fs';
+import path from 'path';
+
 export default async function language(req, res) {
   if (req.method === "GET") {
-    res.status(200);
+    const filePath = path.join(process.cwd(), 'locales', `${req.query.lang}.json`);
+    const data = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+    res.status(200).json(data);
   }
   else {
     res.status(405);
