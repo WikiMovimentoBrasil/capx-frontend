a850e42dee2c477ae58a9724bd2f5d2227534ad7
yuritavares <yuriatp@gmail.com>
2024-05-25 22:36:20 -0300

    Gets language from cookies.

--- src/app/page.js
+++ src/app/page.js
@@ -7,12 +7,13 @@ export default async function Home() {
   const cookieStore = cookies();
   const session = await getServerSession();
   const darkMode = cookieStore.get("dark_mode") === undefined ? "false" : cookieStore.get("dark_mode");
+  const language = cookieStore.get("language") === undefined ? "en-us" : cookieStore.get("language").value;
 
   if (session) {
     redirect('/profile');
   } else {
     return (
-      <ApplicationWrapper session={session !== null} darkMode={darkMode} />
+      <ApplicationWrapper session={session !== null} language={language} darkMode={darkMode} />
     )
   }
 }
