c95bcf53615d1ca99679c1fbcd6712ba5860a74b
yuritavares <yuriatp@gmail.com>
2024-05-25 21:24:21 -0300

    LanguageSelect: Adding component props.

--- src/components/LanguageSelect.js
+++ src/components/LanguageSelect.js
@@ -1,4 +1,4 @@
-export default function LanguageSelect() {
+export default function LanguageSelect({ language, setLanguage, setPageContent }) {
   const options = process.env.NEXT_PUBLIC_LANGUAGES.split(",").map((lang) => ({ value: lang, label: lang }));
 
   return (
