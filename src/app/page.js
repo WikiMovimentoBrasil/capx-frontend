eba199833843c94b26e57b0d81e7d780960d9925
yuritavares <yuriatp@gmail.com>
2024-05-25 22:37:31 -0300

    Loading page data based on the selected language.

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,5 @@
+import fs from 'fs';
+import path from 'path';
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 import { getServerSession } from "next-auth/next";
@@ -9,11 +11,15 @@ export default async function Home() {
   const darkMode = cookieStore.get("dark_mode") === undefined ? "false" : cookieStore.get("dark_mode");
   const language = cookieStore.get("language") === undefined ? "en-us" : cookieStore.get("language").value;
 
+  // Loading page content based on selected language
+  const filePath = path.join(process.cwd(), 'locales', `${language}.json`);
+  const pageContent = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+
   if (session) {
     redirect('/profile');
   } else {
     return (
-      <ApplicationWrapper session={session !== null} language={language} darkMode={darkMode} />
+      <ApplicationWrapper session={session !== null} language={language} pageContent={pageContent} darkMode={darkMode} />
     )
   }
 }
