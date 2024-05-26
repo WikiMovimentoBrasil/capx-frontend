f563d231ce9d3f37e4f3e219cc12bf4d514c97da
yuritavares <yuriatp@gmail.com>
2024-05-25 22:17:24 -0300

    ApplicationWrapper: Passing language-related props.

--- src/components/ApplicationWrapper.js
+++ src/components/ApplicationWrapper.js
@@ -6,11 +6,17 @@ import Section02 from "./Section02";
 
 export default function ApplicationWrapper(props) {
   const [darkMode, setDarkMode] = useState(props.darkMode.value === "true");
+  const [language, setLanguage] = useState(props.language);
   const [mobileMenuStatus, setMobileMenuStatus] = useState(false);
+  const [pageContent, setPageContent] = useState(props.pageContent);
 
   return (
     <BaseWrapper
       session={props.session}
+      language={language}
+      setLanguage={setLanguage}
+      pageContent={pageContent}
+      setPageContent={setPageContent}
       darkMode={darkMode}
       setDarkMode={setDarkMode}
       mobileMenuStatus={mobileMenuStatus}
