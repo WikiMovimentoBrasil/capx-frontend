e27c5346c91a08577a1178ca1d5ade9ae0055ab0
yuritavares <yuriatp@gmail.com>
2024-05-25 21:23:14 -0300

    LanguageSelect: Loading language options.

--- src/components/LanguageSelect.js
+++ src/components/LanguageSelect.js
@@ -1,4 +1,6 @@
 export default function LanguageSelect() {
+  const options = process.env.NEXT_PUBLIC_LANGUAGES.split(",").map((lang) => ({ value: lang, label: lang }));
+
   return (
     <div className="my-auto"></div>
   )
