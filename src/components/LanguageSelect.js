3594958dae3dc2e085d324068c4464993587ac0d
yuritavares <yuriatp@gmail.com>
2024-05-25 21:27:55 -0300

    LanguageSelect: Adding function to handle option selection and to set cookie.

--- src/components/LanguageSelect.js
+++ src/components/LanguageSelect.js
@@ -1,8 +1,18 @@
 import axios from "axios";
 import { useEffect } from "react";
+import { setCookie } from "@/app/actions";
 
 export default function LanguageSelect({ language, setLanguage, setPageContent }) {
   const options = process.env.NEXT_PUBLIC_LANGUAGES.split(",").map((lang) => ({ value: lang, label: lang }));
+
+  const handleSelection = async (selectedOption) => {
+    setLanguage(selectedOption.value);
+    await setCookie({
+      name: "language",
+      value: selectedOption.value,
+      path: '/',
+    });
+  };
 
   useEffect(() => {
     async function getTranslatedContent() {
