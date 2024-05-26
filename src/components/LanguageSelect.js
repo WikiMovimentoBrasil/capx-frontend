533206af77879306497f4b3dc7f2d1022c5d8514
yuritavares <yuriatp@gmail.com>
2024-05-25 21:26:35 -0300

    LanguageSelect: Adding useEffect to query data when language changes.

--- src/components/LanguageSelect.js
+++ src/components/LanguageSelect.js
@@ -1,5 +1,20 @@
+import axios from "axios";
+import { useEffect } from "react";
+
 export default function LanguageSelect({ language, setLanguage, setPageContent }) {
   const options = process.env.NEXT_PUBLIC_LANGUAGES.split(",").map((lang) => ({ value: lang, label: lang }));
+
+  useEffect(() => {
+    async function getTranslatedContent() {
+      const queryResponse = await axios.get("/api/language", {
+        params: {
+          lang: language,
+        }
+      });
+      setPageContent(queryResponse.data);
+    }
+    getTranslatedContent();
+  }, [language]);
 
   return (
     <div className="my-auto"></div>
