823348e57dce0fdb6ae506ea8686ecd025ee38df
yuritavares <yuriatp@gmail.com>
2024-05-26 12:15:33 -0300

    Profile/Edit/MainWrapper: Passing language-related props.

--- src/app/profile/edit/components/MainWrapper.js
+++ src/app/profile/edit/components/MainWrapper.js
@@ -3,12 +3,18 @@ import { useState } from "react";
 import BaseWrapper from "@/components/BaseWrapper";
 
 export default function MainWrapper(props) {
+  const [language, setLanguage] = useState(props.language);
   const [darkMode, setDarkMode] = useState(props.darkMode.value === "true");
   const [mobileMenuStatus, setMobileMenuStatus] = useState(false);
+  const [pageContent, setPageContent] = useState(props.pageContent);
 
   return (
     <BaseWrapper
       session={props.session}
+      language={language}
+      setLanguage={setLanguage}
+      pageContent={pageContent}
+      setPageContent={setPageContent}
       darkMode={darkMode}
       setDarkMode={setDarkMode}
       mobileMenuStatus={mobileMenuStatus}
