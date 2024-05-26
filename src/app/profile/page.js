4b405c541a6e1d9b15d13c8dbc824f0f0a445f5d
yuritavares <yuriatp@gmail.com>
2024-05-26 00:23:17 -0300

    Profile: Loading content (based on the selected language) when page loads.

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -1,3 +1,5 @@
+import fs from 'fs';
+import path from 'path';
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 import { getServerSession } from "next-auth/next";
@@ -7,10 +9,15 @@ export default async function ProfilePage() {
   const cookieStore = cookies();
   const session = await getServerSession();
   const darkMode = cookieStore.get("dark_mode") === undefined ? "false" : cookieStore.get("dark_mode");
+  const language = cookieStore.get("language") === undefined ? "en-us" : cookieStore.get("language").value;
+
+  // Loading page content based on selected language
+  const filePath = path.join(process.cwd(), 'locales', `${language}.json`);
+  const pageContent = JSON.parse(fs.readFileSync(filePath, 'utf8'));
 
   if (session) {
     return (
-      <MainWrapper session={session !== null} darkMode={darkMode} />
+      <MainWrapper session={session !== null} language={language} pageContent={pageContent} darkMode={darkMode} />
     )
   } else {
     redirect('/');
