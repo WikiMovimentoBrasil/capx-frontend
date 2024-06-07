746491eb70e774c4aa467e4d9ecb4677bbedb789
yuritavares <yuriatp@gmail.com>
2024-06-07 11:01:05 -0300

    Profile/Edit/EditProfileForm: Creating function to fetch necessary data.

--- src/app/profile/edit/components/EditProfileForm.js
+++ src/app/profile/edit/components/EditProfileForm.js
@@ -10,4 +10,31 @@ import SubmitButton from "./SubmitButton";
 import LoadingSection from "../../components/LoadingSection";
 import MultiSelectInput from "./MultiSelectInput";
 
+const fetchData = async (queryData) => {
+  try {
+    const [userData, formData, territoryData, languageData, affiliationData, wikiProjectData, skillData] = await Promise.all([
+      axios.get("/api/profile", queryData),
+      axios.options("/api/profile", queryData),
+      axios.get('/api/list/territory', queryData),
+      axios.get('/api/list/language', queryData),
+      axios.get('/api/list/organizations', queryData),
+      axios.get('/api/list/wikimedia_project', queryData),
+      axios.get('/api/list/skills', queryData)
+    ]);
+
+    return {
+      userData: userData.data,
+      formData: formData.data,
+      territoryData: territoryData.data,
+      languageData: languageData.data,
+      affiliationData: affiliationData.data,
+      wikiProjectData: wikiProjectData.data,
+      skillData: skillData.data
+    };
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    return { userData: null, formData: null, affiliationData: null };
+  }
+};
+
 export default function EditProfileForm({ session, language, pageContent, darkMode }) {}
