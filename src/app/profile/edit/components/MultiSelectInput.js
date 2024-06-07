f0ddd337f42473009a61e0f4fb5180df5a63242d
yuritavares <yuriatp@gmail.com>
2024-06-07 10:39:44 -0300

    Profile/Edit/MultiSelectInput: Creating component.

---
+++ src/app/profile/edit/components/MultiSelectInput.js
@@ -0,0 +1,45 @@
+"use client";
+import Select from "react-select";
+import makeAnimated from 'react-select/animated';
+
+const animatedComponents = makeAnimated();
+const multiSelectStyle = {
+  control: (base, state) => ({
+    ...base,
+    height: '48px',
+    paddingLeft: '4px',
+    color: 'rgb(30 41 59)',
+    border: state.isFocused ? 0 : 0,
+    // This line disable the blue border
+    boxShadow: state.isFocused ? 0 : 0,
+    "&:hover": {
+      border: state.isFocused ? 0 : 0
+    },
+  }),
+  placeholder: (styles) => ({
+    ...styles,
+  }),
+};
+
+export default function MultiSelectInput({ children, isUserSelectionLoaded = true, id, options, selectedOptions, onChange }) {
+  if (isUserSelectionLoaded) {
+    return (
+      <div className="w-full space-y-2 mb-8">
+        <label className="ml-2">{children}</label>
+        <Select
+          isMulti
+          name={id}
+          isSearchable={true}
+          styles={multiSelectStyle}
+          placeholder={""}
+          options={options}
+          value={selectedOptions}
+          onChange={onChange}
+          noOptionsMessage={() => "No more options to select."}
+          components={animatedComponents}
+          className="border-2 rounded-md"
+        />
+      </div>
+    )
+  }
+}
