6351da01b7ed94d8dbf5237257288175849cc265
yuritavares <yuriatp@gmail.com>
2024-06-07 10:24:05 -0300

    api/list: Adding endpoint code.

--- src/pages/api/list/[resource].js
+++ src/pages/api/list/[resource].js
@@ -1,6 +1,22 @@
+import axios from "axios";
+
+const acceptedResources = process.env.ACCEPTED_RESOURCES.split(",");
+
 export default async function listResources(req, res) {
   if (req.method === "GET") {
-    res.status(200);
+    const resource = "list_" + req.query.resource;
+    
+    if (acceptedResources.includes(resource)) {
+      const queryResponse = await axios.get(process.env.BASE_URL + "/" + resource, {
+        headers: {
+          'Authorization': req.headers.authorization
+        }
+      });
+      res.status(200).json(queryResponse.data);
+    }
+    else {
+      res.status(400);
+    }
   }
   else {
     res.status(405);
