7853c76e254a4c5a8396edd04ec1dd779b31797a
yuritavares <yuriatp@gmail.com>
2024-06-09 19:25:23 -0300

    Profile/MainWrapper: Fetching more data to render the user profile correctly.

--- src/app/profile/components/MainWrapper.js
+++ src/app/profile/components/MainWrapper.js
@@ -6,6 +6,38 @@ import UserProfile from "./UserProfile";
 import LoadingSection from "./LoadingSection";
 import BaseWrapper from "@/components/BaseWrapper";
 
+const fetchData = async (queryData) => {
+  try {
+    const [userData, territoryData, languageData, affiliationData, wikiProjectData, skillData] = await Promise.all([
+      axios.get("/api/profile", queryData),
+      axios.get('/api/list/territory', queryData),
+      axios.get('/api/list/language', queryData),
+      axios.get('/api/list/organizations', queryData),
+      axios.get('/api/list/wikimedia_project', queryData),
+      axios.get('/api/list/skills', queryData)
+    ]);
+
+    return {
+      userData: userData.data,
+      territoryData: territoryData.data,
+      languageData: languageData.data,
+      affiliationData: affiliationData.data,
+      wikiProjectData: wikiProjectData.data,
+      skillData: skillData.data
+    };
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    return {
+      userData: null,
+      territoryData: null,
+      languageData: null,
+      affiliationData: null,
+      wikiProjectData: null,
+      skillData: null
+    };
+  }
+};
+
 export default function MainWrapper(props) {
   let pageComponent;
   const { status, data } = useSession();
@@ -14,21 +46,22 @@ export default function MainWrapper(props) {
   const [darkMode, setDarkMode] = useState(props.darkMode.value === "true");
   const [mobileMenuStatus, setMobileMenuStatus] = useState(false);
   const [pageContent, setPageContent] = useState(props.pageContent);
+  const [profileData, setProfileData] = useState({ userData: null, territoryData: null, languageData: null, affiliationData: null, wikiProjectData: null, skillData: null });
 
   useEffect(() => {
-    if (status == "authenticated") {
-      async function getUserData() {
-        const queryResponse = await axios.get("/api/profile", {
-          params: {
-            userId: data.user.id,
-          },
-          headers: {
-            'Authorization': `Token ${data.user.token}`,
-          }
-        });
-        setUserData(queryResponse.data);
+    if (status === "authenticated") {
+      const queryData = {
+        params: { userId: data.user.id },
+        headers: { 'Authorization': `Token ${data.user.token}` }
       }
-      getUserData();
+
+      const getData = async (queryData) => {
+        const fetchedData = await fetchData(queryData);
+        setProfileData(fetchedData);
+      };
+
+      getData(queryData);
+
     }
   }, [status]);
 
@@ -37,8 +70,8 @@ export default function MainWrapper(props) {
   }
 
   if (status === "authenticated") {
-    if (Object.keys(userData).length > 0) {
-      pageComponent = (<UserProfile darkMode={darkMode} userData={userData} />);
+    if (profileData.userData != undefined) {
+      pageComponent = (<UserProfile darkMode={darkMode} profileData={profileData} />);
     }
     else {
       pageComponent = (<LoadingSection darkMode={darkMode} message="YOUR PROFILE" />);
