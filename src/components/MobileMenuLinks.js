2153a62eac2168ae842a953aa15e25cd66adefb6
yuritavares <yuriatp@gmail.com>
2024-06-10 17:46:11 -0300

    MobileMenuLinks: Rendering only active links.

--- src/components/MobileMenuLinks.js
+++ src/components/MobileMenuLinks.js
@@ -3,13 +3,13 @@ import { Link } from "react-scroll";
 
 export default function MobileMenuLinks({ session, pageContent, handleMenuStatus }) {
   const menuDataLoggedIn = [
-    { "title": pageContent["navbar-link-profile"], "to": "/profile" },
-    { "title": pageContent["navbar-link-capacities"], "to": "/capacities" },
-    { "title": pageContent["navbar-link-events"], "to": "/events" }
+    { title: pageContent["navbar-link-profile"], to: "/profile", active: true },
+    { title: pageContent["navbar-link-capacities"], to: "/capacities", active: false },
+    { title: pageContent["navbar-link-events"], to: "/events", active: false }
   ]
 
   const menuDataNotLoggedIn = [
-    { title: pageContent["navbar-link-about"], to: "section02" },
+    { title: pageContent["navbar-link-about"], to: "section02", active: true },
   ];
 
   // User is logged in
@@ -17,15 +17,17 @@ export default function MobileMenuLinks({ session, pageContent, handleMenuStatus
     return (
       <div className="flex flex-wrap w-10/12 text-2xl mx-auto space-y-4 mb-8">
         {menuDataLoggedIn.map((item, index) => {
-          return (
-            <NextLink
-              key={"mobile-menu-link-" + index.toString()}
-              href={item.to}
-              className="w-full cursor-pointer border-b py-3"
-            >
-              {item.title}
-            </NextLink>
-          )
+          if (item.active) {
+            return (
+              <NextLink
+                key={"mobile-menu-link-" + index.toString()}
+                href={item.to}
+                className="w-full cursor-pointer border-b py-3"
+              >
+                {item.title}
+              </NextLink>
+            )
+          }
         })}
       </div>
     )
@@ -34,25 +36,27 @@ export default function MobileMenuLinks({ session, pageContent, handleMenuStatus
   return (
     <div className="flex flex-wrap w-10/12 text-2xl mx-auto space-y-4 mb-8">
       {menuDataNotLoggedIn.map((item, index) => {
-        return (
-          <Link
-            key={"mobile-menu-link-" + index.toString()}
-            activeClass="active"
-            to={item.to}
-            spy={true}
-            smooth={true}
-            hashSpy={true}
-            duration={500}
-            delay={150}
-            isDynamic={true}
-            ignoreCancelEvents={false}
-            spyThrottle={500}
-            onClick={handleMenuStatus}
-            className="w-full cursor-pointer border-b py-3"
-          >
-            {item.title}
-          </Link>
-        )
+        if (item.active) {
+          return (
+            <Link
+              key={"mobile-menu-link-" + index.toString()}
+              activeClass="active"
+              to={item.to}
+              spy={true}
+              smooth={true}
+              hashSpy={true}
+              duration={500}
+              delay={150}
+              isDynamic={true}
+              ignoreCancelEvents={false}
+              spyThrottle={500}
+              onClick={handleMenuStatus}
+              className="w-full cursor-pointer border-b py-3"
+            >
+              {item.title}
+            </Link>
+          )
+        }
       })}
     </div>
   )
