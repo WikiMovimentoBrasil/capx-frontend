c0d87f04030d373e2a58c73cf45094db454bb914
yuritavares <yuriatp@gmail.com>
2024-06-10 18:48:41 -0300

    Profile/UserProfile: Showing 'contact' and 'social' only if they hold real data.

--- src/app/profile/components/UserProfile.js
+++ src/app/profile/components/UserProfile.js
@@ -38,18 +38,22 @@ export default function UserProfile({ darkMode, profileData }) {
           </div>
         </div>
         <div className="space-y-10 mb-10">
-          {profileData.userData.contact !== null && profileData.userData.contact.length > 0 ? (
-            <div className="flex flex-wrap place-content-center pt-4">
-              <h2 className="w-full text-center font-extrabold mb-6">Your Contact</h2>
-              {profileData.userData.contact?.map((item, index) => item.display_name !== "" ? (<h3 key={"contact" + index.toString()} className="bg-capx-secondary-purple text-[#F6F6F6] tracking-widest text-xs sm:text-sm px-4 sm:px-5 py-3 rounded-full mb-2 mr-2"><span className="font-extrabold">{item.display_name}</span> | {item.value}</h3>) : (null))}
-            </div>
+          {profileData.userData.contact !== null ? (
+            (profileData.userData.contact.filter((item) => item.display_name !== "" && item.value !== "").length === 0) ? (null) : (
+              <div className="flex flex-wrap place-content-center pt-4">
+                <h2 className="w-full text-center font-extrabold mb-6">Your Contact</h2>
+                {profileData.userData.contact?.map((item, index) => item.display_name !== "" ? (<h3 key={"contact" + index.toString()} className="bg-capx-secondary-purple text-[#F6F6F6] tracking-widest text-xs sm:text-sm px-4 sm:px-5 py-3 rounded-full mb-2 mr-2"><span className="font-extrabold">{item.display_name}</span> | {item.value}</h3>) : (null))}
+              </div>
+            )
           ) : (null)
           }
-          {profileData.userData.social !== null && profileData.userData.social.length > 0 ? (
-            <div className="flex flex-wrap place-content-center pt-4">
-              <h2 className="w-full text-center font-extrabold mb-6">Your Social Media</h2>
-              {profileData.userData.social?.map((item, index) => item.display_name !== "" ? (<h3 key={"social" + index.toString()} className="bg-capx-secondary-purple text-[#F6F6F6] tracking-widest text-xs sm:text-sm px-4 sm:px-5 py-3 rounded-full mb-2 mr-2"><span className="font-extrabold">{item.display_name}</span> | {item.value}</h3>) : (null))}
-            </div>
+          {profileData.userData.social !== null ? (
+            (profileData.userData.social.filter((item) => item.display_name !== "" && item.value !== "").length === 0) ? (null) : (
+              <div className="flex flex-wrap place-content-center pt-4">
+                <h2 className="w-full text-center font-extrabold mb-6">Social Media</h2>
+                {profileData.userData.social?.map((item, index) => item.display_name !== "" ? (<h3 key={"social" + index.toString()} className="bg-capx-secondary-purple text-[#F6F6F6] tracking-widest text-xs sm:text-sm px-4 sm:px-5 py-3 rounded-full mb-2 mr-2"><span className="font-extrabold">{item.display_name}</span> | {item.value}</h3>) : (null))}
+              </div>
+            )
           ) : (null)
           }
         </div>
