9322f87378ad1773db82afcbc7e5eda8ec1c9092
yuritavares <yuriatp@gmail.com>
2024-06-10 13:00:37 -0300

    Profile/UserProfile: Reorganizing code. Adding photo and edit button.

--- src/app/profile/components/UserProfile.js
+++ src/app/profile/components/UserProfile.js
@@ -1,52 +1,32 @@
 import Image from "next/image";
+import ContentBoxSingleColumn from "./ContentBoxSingleColumn";
 import ButtonRedirectToPage from "@/components/ButtonRedirectToPage";
 
-export default function UserProfile({ darkMode, userData }) {
+export default function UserProfile({ darkMode, profileData }) {
+  const pronouns = [
+    { value: "he-him", label: "He/Him" },
+    { value: "she-her", label: "She/Her" },
+    { value: "they-them", label: "They/Them" },
+    { value: "not-specified", label: "Not Specified" },
+    { value: "other", label: "Other" }
+  ]
+
   return (
-    <section className={"w-10/12 mx-auto py-32 place-content-start"}>
-      <div className="w-full text-center mb-8 place-content-center">
+    <section className={"w-10/12 sm:w-8/12 mx-auto py-32 place-content-start"}>
+      <ContentBoxSingleColumn>
         <div>
           <Image
-            src={userData.profile_image ?? 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mP8/x8AAwMCAO+ip1sAAAAASUVORK5CYII='}
+            src={profileData.userData.profile_image ?? 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mP8/x8AAwMCAO+ip1sAAAAASUVORK5CYII='}
             width={200}
             height={200}
             alt="User Profile Photo"
-            className={"mx-auto rounded-full ring-offset-2 ring-8 ring-capx-secondary-purple" + (darkMode ? " ring-offset-capx-dark-box-bg" : " ring-offset-capx-light-box-bg")}
+            className={"mx-auto rounded-full ring-offset-4 ring-8 ring-capx-secondary-purple" + (darkMode ? " ring-offset-capx-dark-box-bg" : " ring-offset-capx-light-box-bg")}
           />
+          <ButtonRedirectToPage to={"/profile/edit"}>
+            Edit Profile
+          </ButtonRedirectToPage>
         </div>
-        <ButtonRedirectToPage to={"/profile/edit"}>
-          Edit Profile
-        </ButtonRedirectToPage>
-        <div className="w-full text-xl space-y-4">
-          <h3>Hello, <span className="font-extrabold">{(userData.display_name && userData.display_name.trim() !== '') ? userData.display_name : userData.user.username}</span>!</h3>
-          <h3>{userData.pronoun}</h3>
-          <h3>{userData.about}</h3>
-          <h3>{userData.wikidata_qid}</h3>
-          <h3>{userData.wiki_alt}</h3>
-          <div className="flex flex-wrap place-content-center pt-4">
-            {userData.territory?.map((item, index) => <h3 key={"territory" + index.toString()} className="bg-capx-secondary-purple text-[#F6F6F6] tracking-widest text-xs sm:text-base px-4 sm:px-5 py-3 rounded-full mb-2 mr-2">{item}</h3>)}
-          </div>
-          <div className="flex flex-wrap place-content-center pt-4">
-            {userData.language?.map((item, index) => <h3 key={"language" + index.toString()} className="bg-capx-secondary-purple text-[#F6F6F6] tracking-widest text-xs sm:text-base px-4 sm:px-5 py-3 rounded-full mb-2 mr-2">{item}</h3>)}
-          </div>
-          <div className="flex flex-wrap place-content-center pt-4">
-            {userData.affiliation?.map((item, index) => <h3 key={"affiliation" + index.toString()} className="bg-capx-secondary-purple text-[#F6F6F6] tracking-widest text-xs sm:text-base px-4 sm:px-5 py-3 rounded-full mb-2 mr-2">{item}</h3>)}
-          </div>
-          <div className="flex flex-wrap place-content-center pt-4">
-            {userData.wikimedia_project?.map((item, index) => <h3 key={"wikimedia_project" + index.toString()} className="bg-capx-secondary-purple text-[#F6F6F6] tracking-widest text-xs sm:text-base px-4 sm:px-5 py-3 rounded-full mb-2 mr-2">{item}</h3>)}
-          </div>
-          <h3>{userData.team}</h3>
-          <div className="flex flex-wrap place-content-center pt-4">
-            {userData.skills_known?.map((item, index) => <h3 key={"skills_known" + index.toString()} className="bg-capx-secondary-purple text-[#F6F6F6] tracking-widest text-xs sm:text-base px-4 sm:px-5 py-3 rounded-full mb-2 mr-2">{item}</h3>)}
-          </div>
-          <div className="flex flex-wrap place-content-center pt-4">
-            {userData.skills_available?.map((item, index) => <h3 key={"skills_available" + index.toString()} className="bg-capx-secondary-purple text-[#F6F6F6] tracking-widest text-xs sm:text-base px-4 sm:px-5 py-3 rounded-full mb-2 mr-2">{item}</h3>)}
-          </div>
-          <div className="flex flex-wrap place-content-center pt-4">
-            {userData.skills_wanted_project?.map((item, index) => <h3 key={"skills_wanted_project" + index.toString()} className="bg-capx-secondary-purple text-[#F6F6F6] tracking-widest text-xs sm:text-base px-4 sm:px-5 py-3 rounded-full mb-2 mr-2">{item}</h3>)}
-          </div>
-        </div>
-      </div>
+      </ContentBoxSingleColumn>
     </section>
   )
 }
