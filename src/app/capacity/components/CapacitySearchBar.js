8378ae2e74df77080f4ba91b747c9fc4ec02f582
yuritavares <yuriatp@gmail.com>
2024-06-13 20:57:38 -0300

    Capacity/CapacitySearchBar: Funtion to handle input change.

--- src/app/capacity/components/CapacitySearchBar.js
+++ src/app/capacity/components/CapacitySearchBar.js
@@ -3,6 +3,19 @@ import { useState } from 'react';
 
 export default function CapacitySearchBar() {
   const [query, setQuery] = useState('');
-  const [filteredMovies, setFilteredMovies] = useState(movies);
+  const [capacityList, setCapacityList] = useState([]);
+  const [filteredCapacityList, setFilteredCapacityList] = useState([]);
+  
+  const handleInputChange = (e) => {
+    const value = e.target.value;
+    setQuery(value);
+
+    // Filtering options based on the query
+    const filtered = capacityList.filter((option) =>
+      option.toLowerCase().includes(value.toLowerCase())
+    );
+    setFilteredCapacityList(filtered);
+  };
+  
   return <div></div>
 }
