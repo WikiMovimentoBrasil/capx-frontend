7510f124c0e57dc8e635c72de973e0f5a303b446
yuritavares <yuriatp@gmail.com>
2024-06-13 20:03:51 -0300

    Skills/MainWrapper: Adding LoadingSection component.

--- src/app/skills/components/MainWrapper.js
+++ src/app/skills/components/MainWrapper.js
@@ -1,6 +1,7 @@
 "use client";
 import { useState } from "react";
 import { useSession } from "next-auth/react";
+import LoadingSection from "@/components/LoadingSection";
 
 export default function MainWrapper(props) {
   const { status, data } = useSession();
@@ -8,6 +9,10 @@ export default function MainWrapper(props) {
   const [darkMode, setDarkMode] = useState(props.darkMode.value === "true");
   const [mobileMenuStatus, setMobileMenuStatus] = useState(false);
   const [pageContent, setPageContent] = useState(props.pageContent);
+
+  if (status === "loading") {
+    return <LoadingSection darkMode={darkMode} message="SKILLS" />
+  }
 
   return <div></div>
 }
