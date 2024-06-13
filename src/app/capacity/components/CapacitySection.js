875860e307d918c4643210e021782968f2f1efa6
yuritavares <yuriatp@gmail.com>
2024-06-13 20:40:11 -0300

    CapacitySection: Renaming component.

--- src/app/capacity/components/CapacitySection.js
+++ src/app/capacity/components/CapacitySection.js
@@ -1,4 +1,4 @@
-export default function SkillSection({ children }) {
+export default function CapacitySection({ children }) {
   return (
     <section className="grid grid-cols-1 w-10/12 sm:w-8/12 py-32 mx-auto">
       {children}
