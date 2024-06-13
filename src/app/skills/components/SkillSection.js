e067bc7105900134b4659de6f8afcacbb58217ea
yuritavares <yuriatp@gmail.com>
2024-06-13 20:34:46 -0300

    SkillSection: Creating component.

---
+++ src/app/skills/components/SkillSection.js
@@ -0,0 +1,7 @@
+export default function SkillSection({ children }) {
+  return (
+    <section className="grid grid-cols-1 w-10/12 sm:w-8/12 py-32 mx-auto">
+      {children}
+    </section>
+  )
+}
