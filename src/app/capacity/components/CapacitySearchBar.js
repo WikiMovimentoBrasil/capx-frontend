90689b4d3468b545bdac7c211010e72b3139584f
yuritavares <yuriatp@gmail.com>
2024-06-13 20:43:49 -0300

    Capacity/CapacitySearchBar: Creting component.

---
+++ src/app/capacity/components/CapacitySearchBar.js
@@ -0,0 +1,8 @@
+"use client"
+import { useState } from 'react';
+
+export default function CapacitySearchBar() {
+  const [query, setQuery] = useState('');
+  const [filteredMovies, setFilteredMovies] = useState(movies);
+  return <div></div>
+}
