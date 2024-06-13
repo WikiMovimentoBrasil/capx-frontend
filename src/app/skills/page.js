8a9b1366360ca7092a95a88982c4ab7e8fe0e5fb
yuritavares <yuriatp@gmail.com>
2024-06-13 17:27:39 -0300

    Skill: Loading language and color theme from cookies.

--- src/app/skills/page.js
+++ src/app/skills/page.js
@@ -1,8 +1,12 @@
+import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 import { getServerSession } from "next-auth/next";
 
 export default async function SkillPage() {
+  const cookieStore = cookies();
   const session = await getServerSession();
+  const darkMode = cookieStore.get("dark_mode") === undefined ? "false" : cookieStore.get("dark_mode");
+  const language = cookieStore.get("language") === undefined ? "en" : cookieStore.get("language").value;
 
   if (session) {
     return (
