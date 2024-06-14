e7de72aa8cc86fc56fc5b0f0311d3f479fa35ebb
yuritavares <yuriatp@gmail.com>
2024-06-13 22:48:57 -0300

    api/capacity: Returning fake data just to test.

--- src/pages/api/capacity.js
+++ src/pages/api/capacity.js
@@ -1,6 +1,15 @@
 export default async function capacity(req, res) {
   if (req.method === "GET") {
-    res.status(200);
+    try {
+      res.status(200).json(
+        {
+          codes: ["QI868829", "QI156335", "QI084535", "QI009950", "QI408880", "QI950523", "QI854288", "QI510796", "QI037437", "QI295487", "QI395971", "QI194607", "QI597943", "QI527631", "QI726398"],
+          names: ["Python", "JavaScript", "Java", "C#", "C++", "Ruby", "Swift", "Go", "Kotlin", "PHP", "TypeScript", "Rust", "Perl", "Scala", "Haskell"]
+        }
+      );
+    } catch (error) {
+      res.status(500).json({ error: "Failed to fetch data." });
+    }
   }
   else {
     res.status(405);
