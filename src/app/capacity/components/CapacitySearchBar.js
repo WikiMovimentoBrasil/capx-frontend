fe6df89326f8bc07b28671a9cfef0a8a185cb857
yuritavares <yuriatp@gmail.com>
2024-06-13 21:19:53 -0300

    Capacity/CapacitySearchBar: Fetching data.

--- src/app/capacity/components/CapacitySearchBar.js
+++ src/app/capacity/components/CapacitySearchBar.js
@@ -1,12 +1,28 @@
 "use client"
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 export default function CapacitySearchBar() {
   const [query, setQuery] = useState('');
   const [capacityNameList, setCapacityNameList] = useState([]);
   const [capacityCodeList, setCapacityCodeList] = useState([]);
-  const [filteredCapacityList, setFilteredCapacityList] = useState([]);
-  
+  const [filteredCapacityList, setFilteredCapacityNameList] = useState([]);
+
+  useEffect(() => {
+    const fetchCapacityList = async () => {
+      try {
+        const response = await fetch('/api/capacity');
+        const data = await response.json();
+        setCapacityCodeList(data.codes);
+        setCapacityNameList(data.names);
+        setFilteredCapacityNameList(data.names);
+      } catch (error) {
+        console.error('Failed to fetch data:', error);
+      }
+    };
+
+    fetchCapacityList();
+  }, []);
+
   const handleInputChange = (e) => {
     const value = e.target.value;
     setQuery(value);
@@ -15,15 +31,15 @@ export default function CapacitySearchBar() {
     const filtered = capacityNameList.filter((option) =>
       option.toLowerCase().includes(value.toLowerCase())
     );
-    setFilteredCapacityList(filtered);
+    setFilteredCapacityNameList(filtered);
   };
 
   const handleOptionClick = (option) => {
     const index = capacityNameList.indexOf(option);
     const code = capacityCodeList[index];
     setQuery(option);
-    setFilteredCapacityList([]);
+    setFilteredCapacityNameList([]);
   };
-  
+
   return <div></div>
 }
