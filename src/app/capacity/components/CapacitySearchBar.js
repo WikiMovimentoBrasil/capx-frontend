898b0b68fa2a52c9265be5e1ca8d98edd0296082
yuritavares <yuriatp@gmail.com>
2024-06-15 23:27:46 -0300

    Capacity/CapacitySearchBar: Disabling search bar when option is selected.

--- src/app/capacity/components/CapacitySearchBar.js
+++ src/app/capacity/components/CapacitySearchBar.js
@@ -1,7 +1,7 @@
 "use client"
 import { useState, useEffect } from 'react';
 
-export default function CapacitySearchBar({ capacityList, setSelectedCapacity, searchBarQuery, setSearchBarQuery, searchBarResultList, setSearchBarResultList, pageContent }) {
+export default function CapacitySearchBar({ capacityList, selectedCapacity, setSelectedCapacity, searchBarQuery, setSearchBarQuery, searchBarResultList, setSearchBarResultList, pageContent }) {
   useEffect(() => {
     setSearchBarResultList(capacityList.names);
   }, [capacityList]);
@@ -57,6 +57,7 @@ export default function CapacitySearchBar({ capacityList, setSelectedCapacity, s
           onChange={handleInputChange}
           className="w-full h-12 text-capx-dark-bg pl-4 border-2 rounded-md"
           placeholder={pageContent["body-capacity-searchbar-placeholder"]}
+          disabled={selectedCapacity.code === "" ? false : true}
         />
         {searchBarQuery && (
           <button
