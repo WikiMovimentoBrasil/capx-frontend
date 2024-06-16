07fc74dde383ec4b028cf4d9fa268fd079939831
yuritavares <yuriatp@gmail.com>
2024-06-15 23:28:26 -0300

    Capacity/CapacitySearchBar: Adding svg as close button.

--- src/app/capacity/components/CapacitySearchBar.js
+++ src/app/capacity/components/CapacitySearchBar.js
@@ -62,9 +62,11 @@ export default function CapacitySearchBar({ capacityList, selectedCapacity, setS
         {searchBarQuery && (
           <button
             onClick={handleClearSearch}
-            className="absolute text-base font-extrabold text-capx-secondary-purple hover:text-gray-400 right-6 top-1/2 transform -translate-y-1/2"
+            className="absolute text-base font-extrabold text-capx-secondary-purple hover:text-gray-400 right-4 top-1/2 transform -translate-y-1/2"
           >
-            x
+            <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18 17.94 6M18 18 6.06 6" />
+            </svg>
           </button>
         )}
       </div>
