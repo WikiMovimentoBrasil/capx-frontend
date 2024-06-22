c8e24432c713abac8b9408fd9efd5890fc3a071d
yuritavares <yuriatp@gmail.com>
2024-06-22 19:27:04 -0300

    Capacity/CapacityProfile: Adding skeleton component (wanted capacities).

--- src/app/capacity/components/CapacityProfile.js
+++ src/app/capacity/components/CapacityProfile.js
@@ -15,50 +15,24 @@ export default function CapacityProfile({ darkMode, selectedCapacity, selectedCa
         <h1 className="text-4xl font-extrabold">{selectedCapacity.name} [<a href={`https://www.wikidata.org/wiki/${selectedCapacity.wd_code}`} target="_blank" className="text-capx-primary-green underline">{selectedCapacity.wd_code}</a>]</h1>
       </div>
       <div className="space-y-14">
-        {selectedCapacityData.wanted?.length > 0 ? (
-          <CapacityUserList
-            darkMode={darkMode}
-            title={pageContent["body-capacity-wanted-user-list-title"]}
-            data={selectedCapacityData.wanted}
-            fetchUserData={fetchUserData}
-          />
+        {selectedCapacityData === undefined ? (
+          skeletonItems
         ) : (
-          <CapacityUserList
-            darkMode={darkMode}
-            title={pageContent["body-capacity-wanted-user-list-title"]}
-            noUserMessage={pageContent["body-capacity-user-list-fallback"]}
-          />
+          selectedCapacityData.wanted?.length > 0 ? (
+            <CapacityUserList
+              darkMode={darkMode}
+              title={pageContent["body-capacity-wanted-user-list-title"]}
+              data={selectedCapacityData.wanted}
+              fetchUserData={fetchUserData}
+            />
+          ) : (
+            <CapacityUserList
+              darkMode={darkMode}
+              title={pageContent["body-capacity-wanted-user-list-title"]}
+              noUserMessage={pageContent["body-capacity-user-list-fallback"]}
+            />
+          )
         )}
-        {selectedCapacityData.known?.length > 0 ? (
-          <CapacityUserList
-            darkMode={darkMode}
-            title={pageContent["body-capacity-known-user-list-title"]}
-            data={selectedCapacityData.known}
-            fetchUserData={fetchUserData}
-          />
-        ) : (
-          <CapacityUserList
-            darkMode={darkMode}
-            title={pageContent["body-capacity-known-user-list-title"]}
-            noUserMessage={pageContent["body-capacity-user-list-fallback"]}
-          />
-        )
-        }
-        {selectedCapacityData.available?.length > 0 ? (
-          <CapacityUserList
-            darkMode={darkMode}
-            title={pageContent["body-capacity-available-user-list-title"]}
-            data={selectedCapacityData.available}
-            fetchUserData={fetchUserData}
-          />
-        ) : (
-          <CapacityUserList
-            darkMode={darkMode}
-            title={pageContent["body-capacity-available-user-list-title"]}
-            noUserMessage={pageContent["body-capacity-user-list-fallback"]}
-          />
-        )
-        }
       </div>
     </div>
   )
