93d7afb925c46758f73af16f686c7e2cf367ecca
yuritavares <yuriatp@gmail.com>
2024-06-22 20:54:38 -0300

    Capacity/CapacityUserPreview: Passing color theme as prop to component.

--- src/app/capacity/components/CapacityUserList.js
+++ src/app/capacity/components/CapacityUserList.js
@@ -6,7 +6,7 @@ export default function CapacityUserList({ darkMode, title, data = undefined, fe
       <h2 className="mb-4">{title}</h2>
       {data ? (
         <div className={(darkMode ? "bg-capx-dark-box-bg " : "bg-capx-light-box-bg ") + "grid grid-cols-10 w-full p-4 rounded-lg"}>
-          {data.map((user, index) => { return (<CapacityUserPreview key={index} userId={user} fetchUserData={fetchUserData} />) })}
+          {data.map((user, index) => { return (<CapacityUserPreview key={index} darkMode={darkMode} userId={user} fetchUserData={fetchUserData} />) })}
         </div>
       ) : (
         <div className={(darkMode ? "bg-capx-dark-box-bg " : "bg-capx-light-box-bg ") + "w-full p-5 border-2 rounded-lg"}>{noUserMessage}</div>
--- src/app/capacity/components/CapacityUserPreview.js
+++ src/app/capacity/components/CapacityUserPreview.js
@@ -1,19 +1,19 @@
 import Link from "next/link";
 import { useEffect, useState } from "react";
 
-export default function CapacityUserPreview({ userId, fetchUserData }) {
-  const [userData, setUserData] = useState({});
+export default function CapacityUserPreview({ darkMode, userId, fetchUserData }) {
+  const [userData, setUserData] = useState(undefined);
 
   useEffect(() => {
     fetchUserData(userId, setUserData);
   }, []);
 
   return (
-    <div className="w-full h-full rounded-full p-4">
+    <div className="w-full h-full p-4 space-y-2">
       <Link href={"/profile/" + userId}>
         <img src={userData.profile_image} className="rounded-full border-2 cursor-pointer" />
       </Link>
-      <p className="text-xs text-center mt-2">{userData.display_name}</p>
+      <p className="text-xs text-center">{userData.display_name}</p>
     </div>
   )
 }
