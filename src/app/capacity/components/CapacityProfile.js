4b564f42ab99b5d2a3ffa6e8bf73e212625e8c52
yuritavares <yuriatp@gmail.com>
2024-06-21 23:54:00 -0300

    Capacity/CapacityProfile: Adding capacity title and wikidata code.

--- src/app/capacity/components/CapacityProfile.js
+++ src/app/capacity/components/CapacityProfile.js
@@ -1,1 +1,11 @@
-export default function CapacityProfile({ selectedCapacity, selectedCapacityData, pageContent, fetchUserData }) {}
+import CapacityUserPreview from "./CapacityUserPreview"
+
+export default function CapacityProfile({ selectedCapacity, selectedCapacityData, pageContent, fetchUserData }) {
+  return (
+    <div className="w-full space-y-8">
+      <div>
+        <h1 className="text-2xl font-extrabold">{selectedCapacity.name} [<a href={`https://www.wikidata.org/wiki/${selectedCapacity.wd_code}`} target="_blank" className="text-capx-primary-green underline">{selectedCapacity.wd_code}</a>]</h1>
+      </div>
+    </div>
+  )
+}
