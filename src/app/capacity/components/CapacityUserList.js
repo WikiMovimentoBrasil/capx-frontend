5c3e35be4c26521ab47720a4f4d70400e3caca6f
yuritavares <yuriatp@gmail.com>
2024-06-22 00:24:44 -0300

    Capacity/CapacityUserList: Adding message if there are no users.

--- src/app/capacity/components/CapacityUserList.js
+++ src/app/capacity/components/CapacityUserList.js
@@ -1,10 +1,17 @@
-export default function CapacityUserList({ title, data }) {
+import CapacityUserPreview from "./CapacityUserPreview";
+
+export default function CapacityUserList({ title, data = undefined, fetchUserData = undefined, noUserMessage = undefined }) {
   return (
     <div>
       <h2 className="mb-4">{title}</h2>
-      <div className="grid grid-cols-10 w-full p-5 bg-gray-100 border-2 rounded-lg">
-        {data.map((user, index) => { return (<CapacityUserPreview userId={user} fetchUserData={fetchUserData} />) })}
-      </div>
-    </div>
+      {data ? (
+        <div className="grid grid-cols-10 w-full p-4 bg-gray-100 border-2 rounded-lg">
+          {data.map((user, index) => { return (<CapacityUserPreview key={index} userId={user} fetchUserData={fetchUserData} />) })}
+        </div>
+      ) : (
+        <div className="w-full bg-gray-100 p-5 border-2 rounded-lg">{noUserMessage}</div>
+      )
+      }
+    </div >
   )
 }
