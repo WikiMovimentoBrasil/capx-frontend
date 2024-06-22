ea0f85cca095c8fa87169b34f6a02b5019fbebfc
yuritavares <yuriatp@gmail.com>
2024-06-22 00:26:24 -0300

    Capacity/CapacityProfile: Adding user list (wanted capacity).

--- src/app/capacity/components/CapacityProfile.js
+++ src/app/capacity/components/CapacityProfile.js
@@ -1,10 +1,19 @@
-import CapacityUserPreview from "./CapacityUserPreview"
+import CapacityUserList from "./CapacityUserList";
 
 export default function CapacityProfile({ selectedCapacity, selectedCapacityData, pageContent, fetchUserData }) {
   return (
-    <div className="w-full space-y-8">
+    <div className="w-full space-y-8 pt-10">
       <div>
-        <h1 className="text-2xl font-extrabold">{selectedCapacity.name} [<a href={`https://www.wikidata.org/wiki/${selectedCapacity.wd_code}`} target="_blank" className="text-capx-primary-green underline">{selectedCapacity.wd_code}</a>]</h1>
+        <h1 className="text-4xl font-extrabold">{selectedCapacity.name} [<a href={`https://www.wikidata.org/wiki/${selectedCapacity.wd_code}`} target="_blank" className="text-capx-primary-green underline">{selectedCapacity.wd_code}</a>]</h1>
+      </div>
+      <div className="space-y-14">
+        {selectedCapacityData.wanted?.length > 0 ? (
+          <CapacityUserList
+            title={"Users that want to learn this capacity"}
+            data={selectedCapacityData.wanted}
+            fetchUserData={fetchUserData}
+          />
+        ) : (null)}
       </div>
     </div>
   )
