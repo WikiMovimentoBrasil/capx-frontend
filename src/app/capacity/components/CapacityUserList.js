4695a62b7bf95695eb804e43c8e8a41c9ac691c0
yuritavares <yuriatp@gmail.com>
2024-06-22 00:00:07 -0300

    Capacity/CapacityUserList: Adding user preview.

--- src/app/capacity/components/CapacityUserList.js
+++ src/app/capacity/components/CapacityUserList.js
@@ -2,6 +2,9 @@ export default function CapacityUserList({ title, data }) {
   return (
     <div>
       <h2 className="mb-4">{title}</h2>
+      <div className="grid grid-cols-10 w-full p-5 bg-gray-100 border-2 rounded-lg">
+        {data.map((user, index) => { return (<CapacityUserPreview userId={user} fetchUserData={fetchUserData} />) })}
+      </div>
     </div>
   )
 }
