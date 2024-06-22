565b61d794e1f83fd85f5cddd8335907cf520409
yuritavares <yuriatp@gmail.com>
2024-06-21 23:39:59 -0300

    Capacity/CapacityUserPreview: Fetching user data.

--- src/app/capacity/components/CapacityUserPreview.js
+++ src/app/capacity/components/CapacityUserPreview.js
@@ -1,1 +1,9 @@
-export default function CapacityUserPreview({ userId, fetchUserData }) {}
+import { useEffect, useState } from "react";
+
+export default function CapacityUserPreview({ userId, fetchUserData }) {
+  const [userData, setUserData] = useState({});
+
+  useEffect(() => {
+    fetchUserData(userId, setUserData);
+  }, []);
+}
