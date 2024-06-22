71c1fda298a9aca981e00b4e7f5def67e5bf96b1
yuritavares <yuriatp@gmail.com>
2024-06-22 12:37:07 -0300

    Profile/[id]: Profile page based on pathname.

---
+++ src/app/profile/[id]/page.js
@@ -0,0 +1,29 @@
+import fs from 'fs';
+import path from 'path';
+import { cookies, headers } from "next/headers";
+import { redirect } from "next/navigation";
+import { getServerSession } from "next-auth/next";
+import MainWrapper from "../components/MainWrapper";
+
+export default async function ProfilePage() {
+  const cookieStore = cookies();
+  const session = await getServerSession();
+  const darkMode = cookieStore.get("dark_mode") === undefined ? "false" : cookieStore.get("dark_mode");
+  const language = cookieStore.get("language") === undefined ? "en" : cookieStore.get("language").value;
+
+  // Loading page content based on selected language
+  const filePath = path.join(process.cwd(), 'locales', `${language}.json`);
+  const pageContent = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+
+  // User ID
+  const pathname = headers().get("x-pathname") || "";
+  const userId = pathname.split("/").slice(-1)[0];
+
+  if (session) {
+    return (
+      <MainWrapper session={session !== null} language={language} darkMode={darkMode} pageContent={pageContent} userId={userId} />
+    )
+  } else {
+    redirect('/');
+  }
+}
