30cbccc33f727cc860f7088bd60cf7a43fe07d72
yuritavares <yuriatp@gmail.com>
2024-06-22 20:55:23 -0300

    Capacity/CapacityUserPreview: Rendering skeleton component while data is requested.

--- src/app/capacity/components/CapacityUserPreview.js
+++ src/app/capacity/components/CapacityUserPreview.js
@@ -8,6 +8,18 @@ export default function CapacityUserPreview({ darkMode, userId, fetchUserData })
     fetchUserData(userId, setUserData);
   }, []);
 
+  if (userData !== undefined) {
+    return (
+      <div className="w-full h-full rounded-full p-4 space-y-2 animate-pulse">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/43/White_square_50%25_transparency.svg/240px-White_square_50%25_transparency.svg.png" className="rounded-full border-2" />
+        <div className="space-y-0.5">
+          <div className={(darkMode ? "bg-capx-dark-bg " : "bg-capx-light-bg ") + "w-full h-4 rounded-lg animate-pulse"}></div>
+          <div className={(darkMode ? "bg-capx-dark-bg " : "bg-capx-light-bg ") + "w-full h-4 rounded-lg animate-pulse"}></div>
+        </div>
+      </div>
+    )
+  }
+
   return (
     <div className="w-full h-full p-4 space-y-2">
       <Link href={"/profile/" + userId}>
