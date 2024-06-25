592b6afca88f16a93fc680e7f36a1a8aec630420
albertoleoncio <albertoleoncio@hotmail.com>
2024-06-25 14:59:00 -0300

    Set wikidata URL directly

--- src/pages/api/capacity/index.js
+++ src/pages/api/capacity/index.js
@@ -20,7 +20,7 @@ export default async function capacity(req, res) {
       const queryTextPart01 = "SELECT ?item ?itemLabel ?itemDescription WHERE {VALUES ?item {";
       const queryTextPart02 = "} SERVICE wikibase:label { bd:serviceParam wikibase:language '" + req.query.language + ",en'.}}";
 
-      const wikidataResponse = await axios.get(process.env.WIKIDATA_BASE_URL + queryTextPart01 + wdCodeList.join(" ") + queryTextPart02);
+      const wikidataResponse = await axios.get('https://query.wikidata.org/bigdata/namespace/wdq/sparql?format=json&query=' + queryTextPart01 + wdCodeList.join(" ") + queryTextPart02);
       const organizedData = wikidataResponse.data.results.bindings.map((wdItem) => {
         return { name: wdItem.itemLabel.value, description: wdItem.itemDescription.value }
       });
