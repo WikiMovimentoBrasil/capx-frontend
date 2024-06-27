7d1d0ab8ead7d17b9565309c0f8eb1718fd93f28
yuritavares <yuriatp@gmail.com>
2024-06-27 00:52:38 -0300

    CapacityProfileUserList: Separating component into two.

--- src/app/capacity/components/CapacityProfileUserList.js
+++ src/app/capacity/components/CapacityProfileUserList.js
@@ -1,17 +1,12 @@
 import CapacityProfileUserPreview from "./CapacityProfileUserPreview";
 
-export default function CapacityProfileUserList({ darkMode, title, data = undefined, noUserMessage = undefined }) {
+export default function CapacityProfileUserList({ darkMode, title, data = undefined, userId }) {
   return (
     <div>
       <h2 className="text-xl font-extrabold mb-4">{title}</h2>
-      {data ? (
-        <div className={(darkMode ? "bg-capx-dark-box-bg " : "bg-capx-light-box-bg ") + "grid grid-cols-10 w-full p-4 rounded-lg"}>
-          {data.map((userData, index) => { return (<CapacityProfileUserPreview key={index} darkMode={darkMode} userData={userData} />) })}
-        </div>
-      ) : (
-        <div className={(darkMode ? "bg-capx-dark-box-bg " : "bg-capx-light-box-bg ") + "w-full p-5 border-2 rounded-lg"}>{noUserMessage}</div>
-      )
-      }
+      <div className={(darkMode ? "bg-capx-dark-box-bg " : "bg-capx-light-box-bg ") + "grid grid-cols-10 w-full p-4 rounded-lg"}>
+        {data.map((userData, index) => { return userData.id !== userId ? (<CapacityProfileUserPreview key={index} darkMode={darkMode} userData={userData} />) : (null) })}
+      </div>
     </div >
   )
 }
