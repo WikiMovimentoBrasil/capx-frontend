57b4aad3c7744c0ff219667052766d2005c9fdd5
yuritavares <yuriatp@gmail.com>
2024-06-27 01:09:14 -0300

    Capacity/CapacityProfileUserList: Removing id verification.

--- src/app/capacity/components/CapacityProfileUserList.js
+++ src/app/capacity/components/CapacityProfileUserList.js
@@ -1,11 +1,11 @@
 import CapacityProfileUserPreview from "./CapacityProfileUserPreview";
 
-export default function CapacityProfileUserList({ darkMode, title, data = undefined, userId }) {
+export default function CapacityProfileUserList({ darkMode, title, data = undefined }) {
   return (
     <div>
       <h2 className="text-xl font-extrabold mb-4">{title}</h2>
       <div className={(darkMode ? "bg-capx-dark-box-bg " : "bg-capx-light-box-bg ") + "grid grid-cols-10 w-full p-4 rounded-lg"}>
-        {data.map((userData, index) => { return userData.id !== userId ? (<CapacityProfileUserPreview key={index} darkMode={darkMode} userData={userData} />) : (null) })}
+        {data.map((userData, index) => { return (<CapacityProfileUserPreview key={index} darkMode={darkMode} userData={userData} />) })}
       </div>
     </div >
   )
