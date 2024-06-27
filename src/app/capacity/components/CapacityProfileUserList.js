225f53a294928702e54c96f3a64e87a04438469c
yuritavares <yuriatp@gmail.com>
2024-06-26 21:53:29 -0300

    Capacity/CapacityProfileUserPreview: Removing the need to request data for each user.

--- src/app/capacity/components/CapacityProfileUserList.js
+++ src/app/capacity/components/CapacityProfileUserList.js
@@ -1,12 +1,12 @@
 import CapacityProfileUserPreview from "./CapacityProfileUserPreview";
 
-export default function CapacityProfileUserList({ darkMode, title, data = undefined, fetchUserData = undefined, noUserMessage = undefined }) {
+export default function CapacityProfileUserList({ darkMode, title, data = undefined, noUserMessage = undefined }) {
   return (
     <div>
       <h2 className="text-xl font-extrabold mb-4">{title}</h2>
       {data ? (
         <div className={(darkMode ? "bg-capx-dark-box-bg " : "bg-capx-light-box-bg ") + "grid grid-cols-10 w-full p-4 rounded-lg"}>
-          {data.map((user, index) => { return (<CapacityProfileUserPreview key={index} darkMode={darkMode} userId={user} fetchUserData={fetchUserData} />) })}
+          {data.map((userData, index) => { return (<CapacityProfileUserPreview key={index} darkMode={darkMode} userData={userData} />) })}
         </div>
       ) : (
         <div className={(darkMode ? "bg-capx-dark-box-bg " : "bg-capx-light-box-bg ") + "w-full p-5 border-2 rounded-lg"}>{noUserMessage}</div>
