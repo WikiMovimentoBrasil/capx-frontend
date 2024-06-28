5cfaf31b3ff0778c7b78b5e4e5db79acd50dc9d4
yuritavares <yuriatp@gmail.com>
2024-06-27 21:01:06 -0300

    Profile/UserProfileView: Adding component 'skeleton'.

--- src/app/profile/components/ContentBoxSingleColumn.js
+++
@@ -1,7 +0,0 @@
-export default function ContentBoxSingleColumn({ children }) {
-  return (
-    <div className="grid grid-cols-1 sm:grid-cols-1 w-full sm:w-8/12 h-fit place-items-center mx-auto text-center text-xl rounded-lg px-12 py-8">
-      {children}
-    </div>
-  )
-}
--- src/app/profile/components/UserProfileView.js
+++ src/app/profile/components/UserProfileView.js
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import ContentBoxSingleColumn from "./ContentBoxSingleColumn";
 import ButtonRedirectToPage from "@/components/ButtonRedirectToPage";
 
 export default function UserProfileView({ darkMode, profileData }) {
@@ -13,13 +12,39 @@ export default function UserProfileView({ darkMode, profileData }) {
 
   if (profileData === undefined) {
     return (
-      <div></div>
+      <section className={"w-10/12 sm:w-8/12 h-screen mx-auto py-32"}>
+        <div className="grid grid-cols-1 sm:grid-cols-1 w-full sm:w-7/12 h-fit mx-auto text-center text-xl space-y-14">
+          <div>
+            <div className={"w-48 h-48 mx-auto rounded-full ring-offset-4 ring-8 animate-pulse" + (darkMode ? " bg-capx-dark-box-bg ring-offset-capx-dark-box-bg ring-capx-dark-box-bg" : " bg-capx-light-box-bg ring-offset-capx-light-box-bg ring-capx-light-box-bg")}></div>
+          </div>
+          <div className="grid w-full place-items-center space-y-10">
+            <div className="w-full space-y-2">
+              <div className={(darkMode ? "bg-capx-dark-box-bg " : "bg-capx-light-box-bg ") + "w-1/2 h-10 mx-auto rounded-lg animate-pulse"}></div>
+              <div className={(darkMode ? "bg-capx-dark-box-bg " : "bg-capx-light-box-bg ") + "w-1/4 h-6 mx-auto rounded-lg animate-pulse"}></div>
+            </div>
+            <div className="w-full space-y-8">
+              <div className="w-full space-y-2">
+                <div className={(darkMode ? "bg-capx-dark-box-bg " : "bg-capx-light-box-bg ") + "w-full h-8 mx-auto rounded-lg animate-pulse"}></div>
+                <div className={(darkMode ? "bg-capx-dark-box-bg " : "bg-capx-light-box-bg ") + "w-3/4 h-8 mx-auto rounded-lg animate-pulse"}></div>
+                <div className={(darkMode ? "bg-capx-dark-box-bg " : "bg-capx-light-box-bg ") + "w-full h-8 mx-auto rounded-lg animate-pulse"}></div>
+                <div className={(darkMode ? "bg-capx-dark-box-bg " : "bg-capx-light-box-bg ") + "w-3/4 h-8 mx-auto rounded-lg animate-pulse"}></div>
+              </div>
+              <div className={(darkMode ? "bg-capx-dark-box-bg " : "bg-capx-light-box-bg ") + "w-1/2 h-8 mx-auto rounded-lg animate-pulse"}></div>
+              <div className={(darkMode ? "bg-capx-dark-box-bg " : "bg-capx-light-box-bg ") + "w-3/4 h-8 mx-auto rounded-lg animate-pulse"}></div>
+            </div>
+            <div className="w-full space-y-2">
+              <div className={(darkMode ? "bg-capx-dark-box-bg " : "bg-capx-light-box-bg ") + "w-1/2 h-8 mx-auto rounded-lg animate-pulse"}></div>
+              <div className={(darkMode ? "bg-capx-dark-box-bg " : "bg-capx-light-box-bg ") + "w-3/4 h-8 mx-auto rounded-lg animate-pulse"}></div>
+            </div>
+          </div>
+        </div>
+      </section>
     )
   }
 
   return (
-    <section className={"w-10/12 sm:w-8/12 mx-auto py-32 place-content-start"}>
-      <ContentBoxSingleColumn>
+    <section className={"w-10/12 sm:w-8/12 mx-auto py-32"}>
+      <div className="grid grid-cols-1 sm:grid-cols-1 w-full sm:w-7/12 h-fit mx-auto text-center text-xl">
         <div>
           <Image
             src={profileData.userData.profile_image ?? 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mP8/x8AAwMCAO+ip1sAAAAASUVORK5CYII='}
@@ -116,7 +141,7 @@ export default function UserProfileView({ darkMode, profileData }) {
           ) : (null)
           }
         </div>
-      </ContentBoxSingleColumn>
+      </div>
     </section>
   )
 }
