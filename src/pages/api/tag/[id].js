599c5aeca44d0bb44372717d97ef8afa665cb692
yuritavares <yuriatp@gmail.com>
2024-07-02 17:16:33 -0300

    api/tag: Requesting list of tag codes.

--- src/pages/api/tag/[id].js
+++ src/pages/api/tag/[id].js
@@ -2,7 +2,15 @@ import axios from "axios";
 
 export default async function getTagData(req, res) {
   if (req.method === "GET") {
-    const { id, category, language } = req.query;
+    const { id, category } = req.query;
+
+    // Requesting list of tag codes (based on chosen category)
+    const codeList = await axios.get(process.env.BASE_URL + `/list_${category}`, {
+      headers: {
+        'Authorization': req.headers.authorization
+      }
+    });
+    res.status(200);
   }
   else {
     res.status(405);
