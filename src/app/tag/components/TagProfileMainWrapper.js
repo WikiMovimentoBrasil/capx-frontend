ee33ac45e1d1a3c726443eab7cabe24f888dabf7
yuritavares <yuriatp@gmail.com>
2024-07-02 18:29:30 -0300

    Tag/TagProfileMainWrapper: Creating function to request tag data.

--- src/app/tag/components/TagProfileMainWrapper.js
+++ src/app/tag/components/TagProfileMainWrapper.js
@@ -1,5 +1,6 @@
 "use client";
-import { useState } from "react";
+import axios from "axios";
+import { useState, useEffect } from "react";
 import { useSession } from "next-auth/react";
 import BaseWrapper from "@/components/BaseWrapper";
 import LoadingSection from "@/components/LoadingSection"
@@ -11,6 +12,15 @@ export default function TagProfileMainWrapper(props) {
   const [mobileMenuStatus, setMobileMenuStatus] = useState(false);
   const [pageContent, setPageContent] = useState(props.pageContent);
   const [selectedTagData, setSelectedTagData] = useState(undefined);
+
+  const getTagData = async (queryData) => {
+    try {
+      const queryResponse = await axios.get(`/api/tag/${props.selectedTagId}`, queryData);
+      setSelectedTagData(queryResponse.data);
+    } catch (error) {
+      console.error('Failed to fetch data:', error);
+    }
+  };
 
   if (status === "loading") {
     return <LoadingSection darkMode={darkMode} message="TAG DATA" />
