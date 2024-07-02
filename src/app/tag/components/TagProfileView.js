403e1f8771fcab5529da980406e88a5b98f8f932
yuritavares <yuriatp@gmail.com>
2024-07-02 18:52:57 -0300

    Tag/TagProfileView: Creating component.

---
+++ src/app/tag/components/TagProfileView.js
@@ -0,0 +1,5 @@
+export default function TagProfileView({darkMode, selectedTagData, pageContent}) {
+  return (
+    <section className="grid grid-cols-1 w-10/12 sm:w-8/12 min-h-screen py-32 place-content-start mx-auto space-y-20"></section>
+  )
+}
