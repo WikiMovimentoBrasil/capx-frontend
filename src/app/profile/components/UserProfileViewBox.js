da28109ebe5cc1983405a300e3f3fcdf8bef6610
yuritavares <yuriatp@gmail.com>
2024-07-01 21:13:25 -0300

    Profile/UserProfileViewBox: Removing unused <div>.

--- src/app/profile/components/UserProfileViewBox.js
+++ src/app/profile/components/UserProfileViewBox.js
@@ -1,9 +1,7 @@
 export default function UserProfileViewBox({ darkMode, title = undefined, info }) {
   return (
-    <div className="w-full space-y-4">
-      <div className={(darkMode ? "bg-capx-dark-box-bg " : "bg-capx-light-box-bg ") + "w-full px-8 py-6 space-y-2 mx-auto rounded-lg break-words"}>
-        {title !== undefined ? (<p><span className="text-xl font-extrabold">{title}</span> | {info}</p>) : (<p>{info}</p>)}
-      </div>
+    <div className={(darkMode ? "bg-capx-dark-box-bg " : "bg-capx-light-box-bg ") + "w-full px-8 py-6 space-y-2 mx-auto rounded-lg break-words"}>
+      {title !== undefined ? (<p><span className="text-xl font-extrabold">{title}</span> | {info}</p>) : (<p>{info}</p>)}
     </div>
   )
 }
