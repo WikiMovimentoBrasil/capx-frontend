97fcdea0143ecdf6bcd775b53e3cd3e51bb80615
yuritavares <yuriatp@gmail.com>
2024-07-02 19:45:18 -0300

    Tag/TagProfileView: Adding 'TagProfileNoUserBox' component if necessary.

--- src/app/tag/components/TagProfileView.js
+++ src/app/tag/components/TagProfileView.js
@@ -1,3 +1,4 @@
+import TagProfileNoUserBox from "./TagProfileNoUserBox";
 import TagProfileViewSkeleton from "./TagProfileViewSkeleton";
 
 export default function TagProfileView({ darkMode, selectedTagData, pageContent }) {
@@ -6,6 +7,15 @@ export default function TagProfileView({ darkMode, selectedTagData, pageContent 
   }
 
   return (
-    <section className="grid grid-cols-1 w-10/12 sm:w-8/12 min-h-screen py-32 place-content-start mx-auto space-y-20"></section>
+    <section className="grid grid-cols-1 w-10/12 sm:w-8/12 min-h-screen py-44 place-content-start mx-auto space-y-20">
+      <h1 className="text-4xl font-extrabold leading-tight">{selectedTagData.name}</h1>
+      {selectedTagData.users?.length === 0 ? (
+        <TagProfileNoUserBox
+          darkMode={darkMode}
+          title={pageContent["body-tag-user-list-title"]}
+          noUserMessage={pageContent["body-tag-user-list-fallback-message"]}
+        />
+      ) : (null)}
+    </section>
   )
 }
