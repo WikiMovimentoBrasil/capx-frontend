1c9174996e3930df08aa0435c5873f230e21b009
yuritavares <yuriatp@gmail.com>
2024-07-02 18:09:58 -0300

    api/tag: Returning data.

--- src/pages/api/tag/[id].js
+++ src/pages/api/tag/[id].js
@@ -4,30 +4,41 @@ export default async function getTagData(req, res) {
   if (req.method === "GET") {
     const { id, category } = req.query;
 
-    // Requesting list of tag codes (based on chosen category)
-    const codeList = await axios.get(process.env.BASE_URL + `/list_${category}`, {
-      headers: {
-        'Authorization': req.headers.authorization
-      }
-    });
-
-    // Returning error if the requested id does not have a corresponding tag code
-    if (codeList.data.hasOwnProperty(id) === false) {
-      res.status(500).json({ error: "No item for this tag id." });
-    }
-
     try {
-      // Requesting the list of users who have the tag in their profile
-      const userList = await axios.get(process.env.BASE_URL + `/tags/${category}/${id}`, {
+      // Requesting list of tag codes (based on chosen category)
+      const codeList = await axios.get(process.env.BASE_URL + `/list_${category}`, {
         headers: {
           'Authorization': req.headers.authorization
         }
       });
-    } catch (error) {
-      res.status(500).json({ error: "Failed to request data." });
-    }
 
-    res.status(200);
+      // Returning error if the requested id does not have a corresponding tag code
+      if (codeList.data.hasOwnProperty(id) === false) {
+        res.status(500).json({ error: "No item for this tag id." });
+      }
+
+      try {
+        // Requesting list of users who have the tag in their profile
+        const userList = await axios.get(process.env.BASE_URL + `/tags/${category}/${id}`, {
+          headers: {
+            'Authorization': req.headers.authorization
+          }
+        });
+
+        const tagData = {
+          code: id,
+          name: codeList.data[id],
+          users: userList.data
+        }
+        res.status(200).json(tagData);
+      }
+      catch (error) {
+        res.status(500).json({ error: "Failed to request list of users." });
+      }
+    }
+    catch (error) {
+      res.status(500).json({ error: "Failed to identify tag." });
+    }
   }
   else {
     res.status(405);
