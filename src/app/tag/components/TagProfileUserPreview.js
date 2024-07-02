fed6bb320639833a8cd4b25200f1e3a8dff4bebe
yuritavares <yuriatp@gmail.com>
2024-07-02 20:18:07 -0300

    Tag/TagProfileUserPreview: Adding component skeleton.

--- src/app/tag/components/TagProfileUserPreview.js
+++ src/app/tag/components/TagProfileUserPreview.js
@@ -1,4 +1,22 @@
 import Link from "next/link";
 import { useEffect, useState } from "react";
 
-export default function TagProfileUserPreview({ darkMode, userData }) {}
+export default function TagProfileUserPreview({ darkMode, userData }) {
+  const [data, setData] = useState(undefined);
+
+  useEffect(() => {
+    setData(userData)
+  }, []);
+
+  if (data === undefined) {
+    return (
+      <div className="w-full h-full rounded-full p-4 space-y-2 animate-pulse">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/43/White_square_50%25_transparency.svg/240px-White_square_50%25_transparency.svg.png" className="rounded-full border-2" />
+        <div className="space-y-0.5">
+          <div className={(darkMode ? "bg-capx-dark-bg " : "bg-capx-light-bg ") + "w-full h-4 rounded-lg animate-pulse"}></div>
+          <div className={(darkMode ? "bg-capx-dark-bg " : "bg-capx-light-bg ") + "w-full h-4 rounded-lg animate-pulse"}></div>
+        </div>
+      </div>
+    )
+  }
+}
