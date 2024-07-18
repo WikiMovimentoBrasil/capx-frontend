942eeb7118663bf54caf0d15f58425c781414e97
albertoleoncio <albertoleoncio@hotmail.com>
2024-07-18 10:29:28 -0300

    feat: improve UI with TreeView

--- src/app/capacity/components/CapacityListMainWrapper.js
+++ src/app/capacity/components/CapacityListMainWrapper.js
@@ -1,12 +1,12 @@
 "use client";
 import axios from "axios";
-import CapacityListView from "./CapacityListView";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useSession } from "next-auth/react";
 import CapacitySection from "./CapacitySection";
 import BaseWrapper from "@/components/BaseWrapper";
 import CapacityListSearchBar from "./CapacityListSearchBar";
 import LoadingSection from "@/components/LoadingSection";
+import { TreeView } from '@primer/react';
 
 export default function CapacityListMainWrapper(props) {
   const { status, data } = useSession();
@@ -15,11 +15,28 @@ export default function CapacityListMainWrapper(props) {
   const [mobileMenuStatus, setMobileMenuStatus] = useState(false);
   const [pageContent, setPageContent] = useState(props.pageContent);
   const [capacityList, setCapacityList] = useState(undefined);
+  const [isLoading, setIsLoading] = useState(false);
+  const [asyncItems, setAsyncItems] = useState([]);
 
   const getCapacityList = async (queryData) => {
     const queryResponse = await axios.get('/api/capacity', queryData);
-    setCapacityList(queryResponse.data);
+    const result = queryResponse.data.reduce((acc, item) => {
+      acc[item.code] = item.name;
+      return acc;
+    }, {});
+    setCapacityList(result);
   };
+
+  // Define const loadItems using API endpoint '/api/capacity/type/' + type
+  const loadItems = async (type) => {
+    const queryData = {
+      headers: {
+        'Authorization': `Token ${data.user.token}`,
+      }
+    }
+    const queryResponse = await axios.get('/api/capacity/type/' + type, queryData);
+    return queryResponse.data;
+  }
 
   useEffect(() => {
     if (status === "authenticated") {
@@ -50,8 +67,32 @@ export default function CapacityListMainWrapper(props) {
     }
   }, [language]);
 
+  const handleExpandedChange = async (isExpanded) => {
+    if (asyncItems.length === 0 && isExpanded) {
+      setIsLoading(true);
+      const items = await loadItems('0');
+
+      const names = {};
+      for (const key in items) {
+        if (capacityList.hasOwnProperty(key)) {
+          names[key] = capacityList[key];
+        }
+      }
+
+      setIsLoading(false);
+      setAsyncItems(names);
+    }
+  };
+
   if (status === "loading") {
     return <LoadingSection darkMode={darkMode} message="CAPACITIES" />
+  }
+
+  let state = 'initial';
+  if (isLoading) {
+    state = 'loading';
+  } else if (asyncItems.length > 0) {
+    state = 'done';
   }
 
   return (
@@ -72,10 +113,26 @@ export default function CapacityListMainWrapper(props) {
           capacityList={capacityList}
           pageContent={pageContent}
         />
-        <CapacityListView
-          darkMode={darkMode}
-          capacityList={capacityList}
-        />
+        <nav aria-label="Files">
+          <TreeView aria-label="Files">
+            <TreeView.Item
+              id="async-directory"
+              onExpandedChange={handleExpandedChange}
+            >
+              <TreeView.LeadingVisual>
+                <TreeView.DirectoryIcon />
+              </TreeView.LeadingVisual>
+              Directory
+              <TreeView.SubTree state={state}>
+                {Object.entries(asyncItems).map(([key, value]) => (
+                  <TreeView.Item id={`item-${key}`} key={key}>
+                    {value}
+                  </TreeView.Item>
+                ))}
+              </TreeView.SubTree>
+            </TreeView.Item>
+          </TreeView>
+        </nav>
       </CapacitySection>
     </BaseWrapper>
   )
