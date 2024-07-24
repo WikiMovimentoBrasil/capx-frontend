0ebf831ef2fdf520b0632ee2398f69df586137e8
= <eder.porto@hotmail.com>
2024-07-24 00:51:18 -0300

    Refactoring BaseWrapper

--- src/components/BaseWrapper.js
+++ src/components/BaseWrapper.js
@@ -3,20 +3,22 @@ import Footer from "@/components/Footer";
 
 export default function BaseWrapper({ children, session, language, setLanguage, pageContent, setPageContent, darkMode, setDarkMode, mobileMenuStatus, setMobileMenuStatus }) {
   return (
-    <main className={(darkMode ? "bg-capx-dark-bg text-capx-light-bg " : "bg-capx-light-bg text-capx-dark-bg ") + (mobileMenuStatus ? "fixed " : "") + " flex flex-wrap flex-col w-full font-montserrat"}>
-      <Navbar
-        session={session}
-        language={language}
-        setLanguage={setLanguage}
-        pageContent={pageContent}
-        setPageContent={setPageContent}
-        darkMode={darkMode}
-        setDarkMode={setDarkMode}
-        mobileMenuStatus={mobileMenuStatus}
-        setMobileMenuStatus={setMobileMenuStatus}
-      />
-      {children}
-      <Footer darkMode={darkMode} pageContent={pageContent} />
-    </main>
+      <div className={"wrapper h-auto sm:h-full " + (darkMode ? "bg-capx-dark-bg text-capx-light-bg " : "bg-capx-light-bg text-capx-dark-bg ")} >
+          <Navbar
+              session={session}
+              language={language}
+              setLanguage={setLanguage}
+              pageContent={pageContent}
+              setPageContent={setPageContent}
+              darkMode={darkMode}
+              setDarkMode={setDarkMode}
+              mobileMenuStatus={mobileMenuStatus}
+              setMobileMenuStatus={setMobileMenuStatus}
+          />
+          <main className="flex flex-wrap flex-col w-full font-montserrat">
+              {children}
+          </main>
+          <Footer darkMode={darkMode} pageContent={pageContent}/>
+      </div>
   )
 }
