197239b8732007fcec13a7c4e9fb79e9c10a3b3e
Victor Góis <victorgois18@gmail.com>
2024-11-19 18:08:19 -0300

    Refact SimpleButton component

---
+++ src/app/(auth)/profile/edit/components/SimpleButton.tsx
@@ -0,0 +1,39 @@
+import Link from "next/link";
+
+interface SimpleButtonProps {
+  children: React.ReactNode;
+  type: "button" | "submit" | "reset";
+  onClick: () => void;
+  to?: string;
+  class_name?: string;
+  bg_color?: string;
+  text_color?: string;
+}
+
+export default function SimpleButton({
+  children,
+  type,
+  onClick,
+  to,
+  class_name,
+  bg_color = "bg-capx-secondary-purple hover:bg-capx-primary-green",
+  text_color = "text-[#F6F6F6] hover:text-capx-dark-bg",
+}: SimpleButtonProps) {
+  return (
+    <button
+      type={type}
+      className={
+        class_name
+          ? class_name
+          : "flex w-fit h-fit tracking-wider text-sm px-4 sm:px-5 py-2 rounded-full " +
+            bg_color +
+            " " +
+            text_color
+      }
+      onClick={onClick}
+      {...(to && { href: to })}
+    >
+      <div>{children}</div>
+    </button>
+  );
+}
