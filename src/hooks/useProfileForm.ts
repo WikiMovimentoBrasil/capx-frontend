59d0124205ec322b506ffa5bff0650d3cf7517dc
Victor Góis <victorgois18@gmail.com>
2024-11-23 09:21:55 -0300

    Create profile hooks

--- src/hooks/useProfileForm.ts
+++ src/hooks/useProfileForm.ts
@@ -1,4 +1,4 @@
-import { useState, useMemo } from "react";
+import { useState } from "react";
 import { useRouter } from "next/navigation";
 import { signOut } from "next-auth/react";
 import axios from "axios";
