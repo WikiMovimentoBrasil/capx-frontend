9bd47a394d2cbd5dad50d6f6449b64c16dcfa45e
albertoleoncio <albertoleoncio@hotmail.com>
2024-12-13 15:51:43 -0300

    Update image component to include width and height attributes

--- src/app/tag/components/TagProfileUserPreview.js
+++ src/app/tag/components/TagProfileUserPreview.js
@@ -12,7 +12,7 @@ export default function TagProfileUserPreview({ darkMode, userData }) {
   if (data === undefined) {
     return (
       <div className="w-full h-full rounded-full p-4 space-y-2 animate-pulse">
-        <Image src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/43/White_square_50%25_transparency.svg/240px-White_square_50%25_transparency.svg.png" className="rounded-full border-2"  alt="white image"/>
+        <Image src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/43/White_square_50%25_transparency.svg/240px-White_square_50%25_transparency.svg.png" className="rounded-full border-2" width={'250'} height={'250'} alt="white image"/>
         <div className="space-y-0.5">
           <div className={(darkMode ? "bg-capx-dark-bg " : "bg-capx-light-bg ") + "w-full h-4 rounded-lg animate-pulse"}></div>
           <div className={(darkMode ? "bg-capx-dark-bg " : "bg-capx-light-bg ") + "w-full h-4 rounded-lg animate-pulse"}></div>
@@ -24,7 +24,7 @@ export default function TagProfileUserPreview({ darkMode, userData }) {
   return (
     <div className="w-full h-full p-4 space-y-2">
       <Link href={"/profile/" + data.id}>
-        <Image src={data.profile_image} className={(darkMode ? "hover:border-capx-light-bg " : "hover:border-capx-dark-bg ") + "rounded-full cursor-pointer hover:border-dotted"} alt="UserProfilePicture"/>
+        <Image src={data.profile_image ?? "https://upload.wikimedia.org/wikipedia/commons/c/cd/CapX-icon.svg"} className={(darkMode ? "hover:border-capx-light-bg " : "hover:border-capx-dark-bg ") + "rounded-full cursor-pointer hover:border-dotted"} width={'250'} height={'250'} alt="UserProfilePicture"/>
       </Link>
       <p className="text-xs text-center">{data.username}</p>
     </div>
