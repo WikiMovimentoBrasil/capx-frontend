a1295b7544cd72737d134e0df9646b6390b40433
Victor Góis <victorgois18@gmail.com>
2024-12-30 15:08:42 -0300

    Change navbar responsive behaviour

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,23 +3,48 @@ import DesktopNavbar from "./DesktopNavbar";
 import MobileNavbar from "./MobileNavbar";
 import { useApp } from "@/contexts/AppContext";
 
-export default function Navbar() {
+interface NavbarProps {
+  session: any;
+  language: string;
+  setLanguage: (language: string) => void;
+  pageContent: any;
+  setPageContent: (pageContent: any) => void;
+  darkMode: boolean;
+  setDarkMode: (darkMode: boolean) => void;
+  mobileMenuStatus: boolean;
+  setMobileMenuStatus: (mobileMenuStatus: boolean) => void;
+  isMobile: boolean;
+}
+
+export default function Navbar({
+  session,
+  language,
+  setLanguage,
+  pageContent,
+  setPageContent,
+}: Omit<
+  NavbarProps,
+  | "darkMode"
+  | "setDarkMode"
+  | "mobileMenuStatus"
+  | "setMobileMenuStatus"
+  | "isMobile"
+>) {
   const {
     isMobile,
-    language,
-    setLanguage,
-    pageContent,
-    setPageContent,
     darkMode,
     setDarkMode,
+    mobileMenuStatus,
+    setMobileMenuStatus,
   } = useApp();
 
   return isMobile ? (
     <MobileNavbar
       language={language}
       setLanguage={setLanguage}
-      pageContent={pageContent}
       setPageContent={setPageContent}
+      session={session}
+      pageContent={pageContent}
     />
   ) : (
     <DesktopNavbar
