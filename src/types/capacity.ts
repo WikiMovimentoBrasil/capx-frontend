6bfb4e304f1b1c6c091aabff18c5f67677d247e3
Victor Góis <victorgois18@gmail.com>
2025-01-09 20:18:32 -0300

    Add new types

--- src/types/capacity.ts
+++ src/types/capacity.ts
@@ -1,39 +1,31 @@
 export interface Capacity {
-  code: string;
-  name: string;
-  wd_code: string;
-  description?: string;
-  users?: {
-    known?: CapacityUser[];
-    available?: CapacityUser[];
-    wanted?: CapacityUser[];
-  };
-}
-
-export interface CapacityUser {
-  id: string;
-  username: string;
-  profile_image?: string;
-}
-
-export interface QueryData {
-  params?: {
-    language?: string;
-    id?: string;
-  };
-  headers: {
-    Authorization: string;
-  };
+  id: number;
+  code?: number;
+  skill_type: string[];
+  skill_wikidata_item: string;
+  name?: string;
+  children?: Capacity[];
 }
 
 export interface CapacityResponse {
   code: string;
   wd_code: string;
   name: string;
-  description?: string;
-  users: {
-    known?: CapacityUser[];
-    available?: CapacityUser[];
-    wanted?: CapacityUser[];
+}
+
+export interface QueryData {
+  params?: {
+    language?: string;
+    [key: string]: any;
   };
+  headers?: {
+    Authorization?: string;
+    [key: string]: any;
+  };
+}
+
+export interface CapacityCategory {
+  id: number;
+  name: string;
+  color: string;
 }
--- src/types/profile.ts
+++ src/types/profile.ts
@@ -1,28 +1,31 @@
+interface Capacity {
+  id: number;
+  skill_type: string[];
+  skill_wikidata_item: string;
+}
+
 export interface Profile {
   name: string;
   about: string;
-  affiliation: string;
+  affiliation: string[];
   contact?: string;
   display_name?: string;
   language: string[];
   profile_image?: string;
   pronoun?: string;
-  skills_available: string[];
-  skills_known: string[];
-  skills_wanted: string[];
+  skills_known: number[];
+  skills_available: number[];
+  skills_wanted: number[];
   social?: string[];
   team?: string;
-  territory?: string;
+  territory?: string[];
   user: {
-    data: {
-      id: string;
-      username: string;
-      email: string;
-      is_active: boolean;
-      is_staff: boolean;
-      last_login: string;
-      date_joined: string;
-    };
+    username: string;
+    email: string;
+    is_active: boolean;
+    is_staff: boolean;
+    last_login: string;
+    date_joined: string;
   };
   wiki_alt?: string;
   wikidata_qid?: string;
