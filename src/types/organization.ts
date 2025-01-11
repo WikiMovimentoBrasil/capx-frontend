f05c09451340d4377ec0f6e74e373ecb8ebe4273
Victor Góis <victorgois18@gmail.com>
2025-01-11 17:04:17 -0300

    Update organization type

--- src/types/organization.ts
+++ src/types/organization.ts
@@ -1,17 +1,24 @@
 export interface Organization {
   id: number;
-  display_name?: string;
+  display_name: string;
   profile_image?: string;
   acronym?: string;
   meta_page?: string;
+  mastodon?: string;
+  tag_diff?: string;
   home_project?: string;
-  updated_date?: string;
+  update_date?: string;
   type?: number;
-  territory?: string[];
-  managers?: string[];
+  territory?: number[];
+  managers?: number[];
   known_capacities?: number[];
   available_capacities?: number[];
   wanted_capacities?: number[];
+  projects?: any[];
+  events?: any[];
+  news?: any[];
+  documents?: any[];
+  contacts?: any[];
 }
 
 export interface OrganizationType {
