60f93b4da99e00e2786e4f19d4da67a8dc87d312
Victor Góis <victorgois18@gmail.com>
2025-02-19 13:25:44 -0300

    Update capacity interface

--- src/types/capacity.ts
+++ src/types/capacity.ts
@@ -4,7 +4,14 @@ export interface Capacity {
   skill_type: string[];
   skill_wikidata_item: string;
   name?: string;
+  icon?: string;
+  color?: string;
   children?: Capacity[];
+  parentCode?: string;
+  description?: string;
+  wd_code?: string;
+  parentCapacity?: Capacity;
+  hasChildren: boolean;
 }
 
 export interface CapacityResponse {
