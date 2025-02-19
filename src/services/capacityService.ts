68789316f0ce502d0509bbc2101addfa5b64a19e
Victor Góis <victorgois18@gmail.com>
2025-02-19 13:26:07 -0300

    Update capacity service

--- src/services/capacityService.ts
+++ src/services/capacityService.ts
@@ -2,7 +2,7 @@ import axios, { AxiosRequestConfig } from "axios";
 import { Capacity, CapacityResponse, QueryData } from "@/types/capacity";
 
 export const capacityService = {
-  async fetchCapacities(queryData: QueryData): Promise<Capacity[]> {
+  async fetchCapacities(queryData: QueryData): Promise<CapacityResponse[]> {
     try {
       const response = await axios.get("/api/capacity", {
         params: queryData.params,
@@ -15,12 +15,31 @@ export const capacityService = {
     }
   },
 
-  fetchCapacityByType: async (type: string, config?: AxiosRequestConfig) => {
+  async fetchCapacitiesByType(
+    type: string,
+    config?: AxiosRequestConfig
+  ): Promise<CapacityResponse[]> {
     try {
       const response = await axios.get(`/api/capacity/type/${type}`, config);
       return response.data;
     } catch (error) {
       console.error("Service - Error:", error);
+      throw error;
+    }
+  },
+
+  async fetchCapacityDescription(
+    code: string,
+    config?: AxiosRequestConfig
+  ): Promise<{ description: string; wdCode: string }> {
+    try {
+      const response = await axios.get(`/api/capacity/${code}`, config);
+      return {
+        description: response.data.description || "",
+        wdCode: response.data.wd_code || "",
+      };
+    } catch (error) {
+      console.error("Failed to fetch capacity description:", error);
       throw error;
     }
   },
@@ -34,6 +53,7 @@ export const capacityService = {
         params: queryData.params,
         headers: queryData.headers,
       });
+
       return response.data;
     } catch (error) {
       console.error("Failed to fetch capacity:", error);
