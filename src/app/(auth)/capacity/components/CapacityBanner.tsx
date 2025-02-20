08e8f543cd434fa6906c9a1323be8608647b7f70
Victor Góis <victorgois18@gmail.com>
2025-02-20 15:38:15 -0300

    Add property priority to decrease LCP

--- src/app/(auth)/capacity/components/CapacityBanner.tsx
+++ src/app/(auth)/capacity/components/CapacityBanner.tsx
@@ -3,10 +3,10 @@ import CapxPeopleLogo from "@/public/static/images/capx_people_illustration.svg"
 
 export function CapacityBanner() {
   return (
-    <section className="flex items-center justify-center h-[399px] w-full bg-capx-dark-bg mb-[40px] rounded-[4px]">
+    <section className="flex items-center justify-center h-[399px] w-full bg-capx-dark-bg rounded-[4px]">
       <div className="flex bg-capx-dark-bg max-w-[1440] gap-[120px] w-full h-full items-center justify-between">
         <div className="relative w-[500px] h-[500px] ml-[120px]">
-          <Image src={CapxPeopleLogo} alt="CapX Logo" fill />
+          <Image src={CapxPeopleLogo} alt="CapX Logo" fill priority />
         </div>
         <div className="flex w-[324px] mr-[120px]">
           <h1 className="text-white text-[48px] font-extrabold leading-[59px]">
