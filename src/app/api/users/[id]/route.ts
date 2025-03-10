86af2bc4e90eb8659e0ba916e9e92f28bab27a4a
Guilherme Gutz <92957528+GuilhermeGutz@users.noreply.github.com>
2025-03-10 15:04:28 -0300

    Create profile by username page (#152)
    * Create profile by username page
    
    * Update text Wiki Movimento Brasil to Wikimedia Brasil
    
    * delete ProfileContet.tsx

--- src/app/api/users/[id]/route.ts
+++ src/app/api/users/[id]/route.ts
@@ -2,26 +2,21 @@ import axios from "axios";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
-  const searchParams = request.nextUrl.searchParams;
+  const userId = request.nextUrl.pathname.split("/").pop();
   const authHeader = request.headers.get("authorization");
-  const search = searchParams.get("search");
 
   try {
     const queryResponse = await axios.get(
-      `${process.env.BASE_URL}/users/`,
+      `${process.env.BASE_URL}/users/${userId}`,
       {
         headers: {
           Authorization: authHeader,
         },
-        params: {
-          search,
-        },
       }
     );
-    // get one user by search
-    return NextResponse.json(queryResponse.data[0]);
+
+    return NextResponse.json(queryResponse.data);
   } catch (error) {
-    console.error("Failed to fetch user by id", error);
     return NextResponse.json(
       { error: "Failed to fetch user data" },
       { status: 500 }
--- src/services/userService.ts
+++ src/services/userService.ts
@@ -2,9 +2,17 @@ import axios from "axios";
 
 export const userService = {
   async fetchUserProfile(userId: string, token: string) {
-    const response = await axios.get(`/api/users`, {
+    const response = await axios.get(`/api/users/${userId}`, {
+      headers: {
+        Authorization: `Token ${token}`,
+      },
+    });
+    return response.data;
+  },
+  async fetchAllUsers(token: string, search?: string) {
+    const response = await axios.get(`/api/users/`, {
       params: {
-        userId,
+        search,
       },
       headers: {
         Authorization: `Token ${token}`,
