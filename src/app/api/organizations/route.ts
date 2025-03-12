39f82167b8a8eae9454004d95eb64eaf6f5fe4aa
Victor Góis <victorgois18@gmail.com>
2025-03-12 16:19:41 -0300

    Update organization with new params

--- src/app/api/organizations/route.ts
+++ src/app/api/organizations/route.ts
@@ -3,14 +3,21 @@ import axios from "axios";
 
 export async function GET(request: NextRequest) {
   const authHeader = request.headers.get("authorization");
+  const searchParams = request.nextUrl.searchParams;
+  const limit = searchParams.get("limit");
+  const offset = searchParams.get("offset");
 
   try {
     const response = await axios.get(`${process.env.BASE_URL}/organizations/`, {
       headers: {
         Authorization: authHeader,
       },
+      params: {
+        limit,
+        offset,
+      },
     });
-    return NextResponse.json(response.data);
+    return NextResponse.json(response.data.results);
   } catch (error: any) {
     console.error("Error details:", {
       status: error.response?.status,
