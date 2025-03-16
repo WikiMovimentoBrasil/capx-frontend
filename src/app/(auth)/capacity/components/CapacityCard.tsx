81232f8346b7299d1e908e070c8a1a5f24cd764d
Leticia Nascimento <leticianascimento.dev@gmail.com>
2025-03-16 20:51:23 -0300

    Fix feed redirect

--- src/app/(auth)/capacity/components/CapacityCard.tsx
+++ src/app/(auth)/capacity/components/CapacityCard.tsx
@@ -218,7 +218,7 @@ export function CapacityCard({
               }`}
             >
               <div className="flex items-center w-[378px] h-full">
-                <Link href={`/?capacityId=${code}`}>
+                <Link href={`/feed?capacityId=${code}`}>
                   <h3
                     className={`font-extrabold text-white ${
                       isMobile ? "text-[20px]" : "text-[48px]"
